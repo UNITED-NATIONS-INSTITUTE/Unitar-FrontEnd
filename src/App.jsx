2ddf38e78f46c6e6bfe66ba4ce1b495b0fab778d
Isaranwa <pennyisaranwa@gmail.com>
2023-11-22 21:52:39 +0300

    Add Route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./components/common/LandingPage";
+import SignUp from "./components/common/SignUp";
+import LogIn from "./components/common/LogIn";
+import NoPage from "./components/common/NoPage";
 
 const App = () => {
-  return <h1>hello</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
