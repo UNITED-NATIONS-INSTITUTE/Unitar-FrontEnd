11b008fc15a29114e05be09650b4c61e90b64417
rachealmuchiri <rachealloveo6@gmail.com>
2023-11-22 23:29:09 +0300

    added the navbar and renamed logo

--- src/components/common/LandingPage.jsx
+++ src/components/common/LandingPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import Navbar from "./Navbar";
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default LandingPage;
---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/unitar-logo.png";
+const Nav = () => {
+  let [open, setOpen] = useState(false);
+  return (
+    <div className="shadow-md w-full fixed top-0 left-0 ">
+      <div className="md:flex items-center justify-between bg-white py-2 md:px-10 px-7">
+        <div
+          className="  cursor-pointer flex items-center  
+      "
+        >
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="flex justify-between items-center gap-[120px] ">
+          <Link to="/login">
+            <button className=" rounded-[8px] flex-col items-center justify-center gap-10 p-2 w-83 h-50 flex-shrink-0 rounded-10 border-2 border-blue-500">
+              LogIn
+            </button>
+          </Link>
+          <Link to="/signup">
+            <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-blue-500">
+              Sign Up
+            </button>
+          </Link>
+        </div>
+        <div className="md:hidden" onClick={handleNav}>
+          <AiOutlineMenu size={25} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Nav;
