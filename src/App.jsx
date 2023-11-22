eac68b24ada41e55a86664bda8fb45d006c931b6
Isaranwa <pennyisaranwa@gmail.com>
2023-11-22 21:25:59 +0300

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-function App() {
-  return (
-    <h1 className="w-full h-screen text-4xl text-blue-700 flex items-center justify-center">
-      Hello tailwind
-    </h1>
-  );
-}
+import React from "react";
+
+const App = () => {
+  return <h1>hello</h1>;
+};
 
 export default App;
---
+++ src/components/participants/ParticipantsPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ParticipantsPage = () => {
+  return <div>ParticipantsPage</div>;
+};
+
+export default ParticipantsPage;
