0ef5525ce120da6a69edd117de8672f010b013ed
Isaranwa <pennyisaranwa@gmail.com>
2023-11-22 19:45:39 +0300

    Add the hackathon, participant, organizers slice

--- src/store/store.js
+++ src/store/store.js
@@ -3,31 +3,37 @@ import storage from "redux-persist/lib/storage";
 import { persistReducer, persistStore } from "redux-persist";
 import thunk from "redux-thunk";
 import userSlice from "../features/user/userSlice";
+import participantSlice from "../features/participant/participantSlice";
+import organizersSlice from "../features/organizers/organizersSlice";
+import hackathonSlice from "../features/hackathon/hackathonSlice";
 
 const persistConfig = {
-    key: "root",
-    storage,
+  key: "root",
+  storage,
 };
 
 const appReducer = combineReducers({
-    // your apps' top-level reducers
-    user: userSlice,
-})
+  // your apps' top-level reducers
+  user: userSlice,
+  participant: participantSlice,
+  organizer: organizersSlice,
+  hackathon: hackathonSlice,
+});
 
 const rootReducer = (state, action) => {
-    if(action.type === "USER_LOGOUT") {
-        storage.removeItem('persist:root')
-        return appReducer(undefined, action)
-    }
-    return appReducer(state, action)
-}
+  if (action.type === "USER_LOGOUT") {
+    storage.removeItem("persist:root");
+    return appReducer(undefined, action);
+  }
+  return appReducer(state, action);
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: persistedReducer,
-    devTools: process.env.NODE_ENV !== "production",
-    middleware: [thunk],
-})
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: [thunk],
+});
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
