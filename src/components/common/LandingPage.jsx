1f980a449eb67a76d491bec48cf362743341152c
Isaranwa <pennyisaranwa@gmail.com>
2023-11-23 10:52:51 +0300

    Add sign up page

--- src/components/common/LandingPage.jsx
+++ src/components/common/LandingPage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Navbar from "./Navbar";
+import LogIn from "./LogIn";
 const LandingPage = () => {
   return (
     <div>
-      <Navbar />
+      <LogIn />
     </div>
   );
 };
