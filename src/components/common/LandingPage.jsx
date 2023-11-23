65ac9a33c24c3376b4be0f4d9128e65982ad3fd8
Isaranwa <pennyisaranwa@gmail.com>
2023-11-23 18:10:43 +0300

    Add the dashboard component

--- src/components/common/LandingPage.jsx
+++ src/components/common/LandingPage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Dashboard from "../participants/Dashboard";
 
 const LandingPage = () => {
-  return <div>landing page</div>;
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
 };
 
 export default LandingPage;
