b54fc56ed16eabcd995cbd322da08ab0a15ae198
Isaranwa <pennyisaranwa@gmail.com>
2023-11-24 12:21:47 +0300

    Add border to login button

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -14,26 +14,15 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
 
-        <div className="flex justify-between items-center gap-[50px] ">
+        <div className="flex justify-between items-center gap-[100px] ">
           <Link to="/login">
-            <button className=" rounded-[8px] flex-col items-center justify-center gap-10 p-2 w-83 h-50 flex-shrink-0  border-2 border-custom-blue">
-              LogIn
+            <button className=" btn-login inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 ">
+              Login
             </button>
           </Link>
-          <Link to="/signup">
-
-            <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-custom-blue" />
-
+          <Link to="/prompt">
             {/* TO DO: question if user wanna sign in as org or part, BY RAISING MODAL */}
-            <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-blue-500">
-
-            <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0  bg-custom-blue"
-
             <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-custom-blue">
-
-            {/* TO DO: question if user wanna sign in as org or part, BY RAISING MODAL */}
-            <button className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-blue-500">
-
               Sign Up
             </button>
           </Link>
