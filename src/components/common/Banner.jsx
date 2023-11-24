b28c9a369a394bfb3662c87d6e5390ad4f5c6751
rachealmuchiri <rachealloveo6@gmail.com>
2023-11-24 17:25:56 +0300

    added the logos

---
+++ src/components/common/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <section className=" my-4 w-[1600px] h-[472px] ">
+      <div className=" flex-wrap w-full flex justify-center items-center"></div>
+    </section>
+  );
+};
+
+export default Banner;
