21cff16260699735508ee748fb92856e88a96252
steve <austines811@gmail.com>
2023-11-26 18:39:33 +0300

    refactors routes to standard

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./components/common/LandingPage";
 import SignUp from "./components/common/SignUp";
 import LogIn from "./components/common/LogIn";
@@ -18,37 +18,34 @@ import CreateHackathon from "./components/organizers/createhackathon/CreateHacka
 
 const App = () => {
   return (
-    // WRAP BROWSER ROUTER IN INDEX.
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<NoPage />} />
-        {/* COMMON ROUTES */}
-        <Route index element={<LandingPage />} />
-        <Route path="/org-signup" element={<SignUp />} />
-        <Route path="/part-signup" element={<SignUp />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/profile" element={<Profile />} />
-        {/* PARTICIPANT */}
-        {/* The parent element should be a Nav component that allows an outlet its the side. Thus refactor 24 below */}
-        <Route path="participant" element={<Sidebar />}>
-          <Route index element={<ParticipantDashboard />} />
-          <Route path="part-dashboard" element={<ParticipantDashboard />} />
-          <Route path="hackathons" element={<HackathonDashboard />} />
-          <Route path="hack-details" element={<HackathonDetailsPage />} />
-          <Route path="submit-project" element={<SubmitHackathon />} />
-          {/* Other participant routes cascade here: if a subroute, wrap and cascade */}
+    <Routes>
+      <Route path="*" element={<NoPage />} />
+      {/* COMMON ROUTES */}
+      <Route index element={<LandingPage />} />
+      <Route path="/org-signup" element={<SignUp />} />
+      <Route path="/part-signup" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
+      <Route path="/profile" element={<Profile />} />
+      {/* PARTICIPANT */}
+      <Route path="participant" element={<Sidebar />}>
+        <Route index element={<ParticipantDashboard />} />
+        <Route path="dashboard" element={<ParticipantDashboard />} />
+        <Route path="hackathons" element={<HackathonDashboard />}>
+          <Route path="detail" element={<HackathonDetailsPage />} />
+          <Route path="submit" element={<SubmitHackathon />} />
         </Route>
-        {/* ORGANIZER */}
-        <Route path="organizer" element={<OrgSidebar />}>
-          <Route index element={<OrgDashboard />} />
-          <Route path="org-dashboard" element={<OrgDashboard />} />
-          <Route path="org-hackathon" element={<OrgHackathonPage />} />
-          <Route path="org-submissions" element={<OrgSubmissionPage />} />
+      </Route>
+      {/* ORGANIZER */}
+      <Route path="organizer" element={<OrgSidebar />}>
+        <Route index element={<OrgDashboard />} />
+        <Route path="dashboard" element={<OrgDashboard />} />
+        <Route path="hackathons" element={<OrgHackathonPage />}>
+          <Route path="/create" element={<CreateHackathon />} />
         </Route>
-        <Route path="/create-hackathon" element={<CreateHackathon />} />
-        ADMIN
-      </Routes>
-    </BrowserRouter>
+        <Route path="submissions" element={<OrgSubmissionPage />} />
+      </Route>
+      {/* ADMIN */}
+    </Routes>
   );
 };
 
