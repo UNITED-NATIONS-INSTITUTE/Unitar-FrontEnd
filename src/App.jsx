dfda6bc514a6c3ae45c67d52b4c8b9dfdb408ca0
steve <austines811@gmail.com>
2023-11-26 19:55:21 +0300

    refactors hackathon and profile organization

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,17 @@ import SignUp from "./components/common/SignUp";
 import LogIn from "./components/common/LogIn";
 import NoPage from "./components/common/NoPage";
 import ParticipantDashboard from "./components/participants/ParticipantDashboard";
-import Profile from "./components/common/profile/Profile";
 import Sidebar from "./components/participants/Sidebar";
 import HackathonDetailsPage from "./components/hackathon/submitProject/HackathonDetailsPage";
-import HackathonDashboard from "./components/hackathon/hackathonDashboard/HackathonDashboard";
+import HackathonDashboard from "./components/hackathon/hackathonDashboard/HackathonDashboard.jsx";
 import SubmitHackathon from "./components/hackathon/submitHackathon/SubmitHackathon";
 import OrgDashboard from "./components/organizers/OrgDashboard";
 import OrgSidebar from "./components/organizers/OrgSidebar";
 import OrgHackathonPage from "./components/organizers/OrgHackathonPage";
 import OrgSubmissionPage from "./components/organizers/OrgSubmissionPage";
 import CreateHackathon from "./components/organizers/createhackathon/CreateHackathon";
-
+// profile
+import ProfileDetails from "./components/common/profile/ProfileDetails";
 const App = () => {
   return (
     <Routes>
@@ -25,12 +25,14 @@ const App = () => {
       <Route path="/org-signup" element={<SignUp />} />
       <Route path="/part-signup" element={<SignUp />} />
       <Route path="/login" element={<LogIn />} />
-      <Route path="/profile" element={<Profile />} />
+
       {/* PARTICIPANT */}
       <Route path="participant" element={<Sidebar />}>
         <Route index element={<ParticipantDashboard />} />
+        <Route path="profile" element={<ProfileDetails />} />
         <Route path="dashboard" element={<ParticipantDashboard />} />
-        <Route path="hackathons" element={<HackathonDashboard />}>
+        <Route path="hackathons" element={null}>
+          <Route index element={<HackathonDashboard />} />
           <Route path="detail" element={<HackathonDetailsPage />} />
           <Route path="submit" element={<SubmitHackathon />} />
         </Route>
@@ -38,6 +40,7 @@ const App = () => {
       {/* ORGANIZER */}
       <Route path="organizer" element={<OrgSidebar />}>
         <Route index element={<OrgDashboard />} />
+        <Route path="profile" element={<ProfileDetails />} />
         <Route path="dashboard" element={<OrgDashboard />} />
         <Route path="hackathons" element={<OrgHackathonPage />}>
           <Route path="create" element={<CreateHackathon />} />
