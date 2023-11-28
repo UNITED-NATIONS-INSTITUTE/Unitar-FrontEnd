8d28f03c2cb9326b226cfa5189e58af3b56f5622
Isaranwa <pennyisaranwa@gmail.com>
2023-11-28 16:46:36 +0300

    Remove table borders

--- src/components/organizers/OrgSubmissionsTable.jsx
+++ src/components/organizers/OrgSubmissionsTable.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const OrgSubmissionsTable = () => {
+  const customBorder = {
+    border: "none",
+    borderBottom: "1px solid #0e0e0e",
+  };
   const columns = [
-    { field: "participant", headerName: "Participant", width: 130 },
-    { field: "projectname", headerName: "Project name", width: 200 },
-    { field: "github", headerName: "Github", width: 200 },
-    { field: "presentationlink", headerName: "Presentation link", width: 200 },
-    { field: "livelink", headerName: "Live link", width: 200 },
+    {
+      field: "profileImage",
+      headerName: "",
+      width: 20,
+      renderCell: () => (
+        <img
+          src="/assets/avatar1.jpg"
+          alt="profile"
+          style={{ borderRadius: "50%", width: "32px", height: "32px" }}
+        />
+      ),
+    },
+    { field: "participant", headerName: "Participant", width: 140 },
+    { field: "projectName", headerName: "Project name", width: 180 },
+    { field: "github", headerName: "Github", width: 180 },
+    { field: "presentationLink", headerName: "Presentation link", width: 180 },
+    { field: "liveLink", headerName: "Live link", width: 180 },
     {
       field: "action",
       headerName: "Action",
@@ -23,16 +39,18 @@ const OrgSubmissionsTable = () => {
   const rows = [
     {
       id: 1,
+      profileImage: "URL_TO_PARTICIPANT_IMAGE",
       participant: "Dennis Kim",
-      projectname: "Hackathon project",
+      projectName: "Hackathon project",
       github: "http://www.github.com",
-      presentationlink: "http://www.netlify.com",
-      livelink: "http://www.netlify.com",
+      presentationLink: "http://www.netlify.com",
+      liveLink: "http://www.netlify.com",
     },
   ];
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
+        sx={customBorder}
         rows={rows}
         columns={columns}
         initialState={{
