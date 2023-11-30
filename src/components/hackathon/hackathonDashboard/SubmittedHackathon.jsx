8e6767cabe854b60a0e1411f5927a127d6da0d88
Isaranwa <pennyisaranwa@gmail.com>
2023-11-30 14:13:49 +0300

    Remove link add useNavigate

--- src/components/hackathon/hackathonDashboard/SubmittedHackathon.jsx
+++ src/components/hackathon/hackathonDashboard/SubmittedHackathon.jsx
@@ -1,9 +1,7 @@
 import React from "react";
 import Avatars from "../../common/Avatars";
-import { Link, useNavigate } from "react-router-dom";
 
 const SubmittedHackathon = () => {
-  const navigate = useNavigate();
   const images = [{ name: "project #1", url: "image1.svg" }];
   return (
     <div className="flex space-x-4 mt-5 ml-4">
@@ -24,13 +22,9 @@ const SubmittedHackathon = () => {
             working torwards SDGs
           </p>
           <div className="flex gap-5">
-            <button
-              onClick={() => navigate("/participant/hackathons/detail")}
-              className="border border-blue-500 rounded-md text-blue-500 w-[100px] text-xs mt-4 py-1"
-            >
+            <button className="border border-blue-500 rounded-md text-blue-500 w-[100px] text-xs mt-4 py-1">
               View project
             </button>
-
             <Avatars />
           </div>
         </div>
