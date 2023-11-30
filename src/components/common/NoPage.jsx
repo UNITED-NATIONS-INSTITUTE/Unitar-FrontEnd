f9a40c22f1445412b1cdfde910a557e7b88c901d
steve <austines811@gmail.com>
2023-11-30 13:00:37 +0300

    adds back button functionality

--- src/components/common/NoPage.jsx
+++ src/components/common/NoPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const NoPage = () => {
+  const navigate = useNavigate()
   return (
     <div className="bg-white min-h-screen overflow-hidden">
       <div>
@@ -18,7 +19,7 @@ const NoPage = () => {
             src="/assets/404 image.jpg"
             alt="404 page"
           />
-          <button className=" border-2 border-custom-blue rounded-md px-10 py-2 w-[150px] font-semibold text-sm text-custom-blue">
+          <button onClick={() => navigate(-1)} className=" border-2 border-custom-blue rounded-md px-10 py-2 w-[150px] font-semibold text-sm text-custom-blue">
             Back
           </button>
         </div>
