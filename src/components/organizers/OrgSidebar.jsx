e21b95b9039a130557e37f62d0597fac94e10739
steve <austines811@gmail.com>
2023-11-30 12:30:31 +0300

    refactors highlight and navigation

--- src/components/organizers/OrgSidebar.jsx
+++ src/components/organizers/OrgSidebar.jsx
@@ -1,11 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../assets/unitar-logo.svg";
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 const OrgSidebar = () => {
   const [activePage, setActivePage] = useState("Dashboard");
-  const handleClickPage = (page) => {
-    setActivePage(page);
-  };
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathnameArray = location.pathname.split("/");
+  console.log(activePage);
+  useEffect(() => {
+    if (pathnameArray[2] === undefined || pathnameArray[2] === "dashboard") {
+      setActivePage("dashboard");
+    } else if (pathnameArray[2] === "hackathons") {
+      setActivePage("hackathons");
+    } else if (pathnameArray[2] === "submissions") {
+      setActivePage("submissions");
+    }
+  }, [pathnameArray]);
   return (
     <div className=" flex ">
       <div className="bg-light-blue left-side  p-5 h-screen fixed left-0 top-0 ">
@@ -13,67 +24,55 @@ const OrgSidebar = () => {
           <img src={logo} alt="logo" width="100" height="54" />
           <img src="/assets/back.svg" alt="back" width="20" height="20" />
         </div>
-        <Link to="/organizer/org-dashboard">
-          <button
-            onClick={() => handleClickPage("Dashboard")}
-            style={{
-              backgroundColor:
-                activePage === "Dashboard" ? "#089BD9" : "inherit",
-            }}
-            className="py-2 px-6 border rounded-md hover:bg-custom-blue mt-16  "
-          >
-            <div className="flex gap-5">
-              <img
-                src="/assets/dashboard.svg"
-                alt="icon"
-                width="20"
-                height="20"
-              />
-              <span>Dashboard</span>
-            </div>
-          </button>
-        </Link>
+        <button
+          onClick={() => navigate("/organizer/dashboard")}
+          style={{
+            backgroundColor: activePage === "dashboard" ? "#089BD9" : "inherit",
+          }}
+          className="py-2 px-6 border rounded-md hover:bg-custom-blue mt-16  "
+        >
+          <div className="flex gap-5">
+            <img
+              src="/assets/dashboard.svg"
+              alt="icon"
+              width="20"
+              height="20"
+            />
+            <span>Dashboard</span>
+          </div>
+        </button>
 
-        <Link to="/organizer/org-hackathon">
-          <button
-            onClick={() => handleClickPage("Hackathon")}
-            style={{
-              backgroundColor:
-                activePage === "Hackathon" ? "#089BD9" : "inherit",
-            }}
-            className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
-          >
-            <div className="flex gap-5">
-              <img
-                src="/assets/hackathon.svg"
-                alt="icon"
-                width="20"
-                height="20"
-              />
-              <span>Hackathon</span>
-            </div>
-          </button>
-        </Link>
-        <Link to="/organizer/org-submissions">
-          <button
-            onClick={() => handleClickPage("Submission")}
-            style={{
-              backgroundColor:
-                activePage === "Submission" ? "#089BD9" : "inherit",
-            }}
-            className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
-          >
-            <div className="flex gap-5">
-              <img
-                src="/assets/projects.svg"
-                alt="icon"
-                width="20"
-                height="20"
-              />
-              <span>Submissions</span>
-            </div>
-          </button>
-        </Link>
+        <button
+          onClick={() => navigate("/organizer/hackathons")}
+          style={{
+            backgroundColor:
+              activePage === "hackathons" ? "#089BD9" : "inherit",
+          }}
+          className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
+        >
+          <div className="flex gap-5">
+            <img
+              src="/assets/hackathon.svg"
+              alt="icon"
+              width="20"
+              height="20"
+            />
+            <span>Hackathon</span>
+          </div>
+        </button>
+        <button
+          onClick={() => navigate("/organizer/submissions")}
+          style={{
+            backgroundColor:
+              activePage === "submissions" ? "#089BD9" : "inherit",
+          }}
+          className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
+        >
+          <div className="flex gap-5">
+            <img src="/assets/projects.svg" alt="icon" width="20" height="20" />
+            <span>Submissions</span>
+          </div>
+        </button>
       </div>
       <Outlet />
     </div>
