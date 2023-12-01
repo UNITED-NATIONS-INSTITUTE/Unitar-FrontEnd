3b6771ecb1aebf917f1a330768cf142b1de51d29
steve <austines811@gmail.com>
2023-12-01 15:18:42 +0300

    passes an index prop to make picker aware of entry

--- src/components/common/utils/DatePicker.jsx
+++ src/components/common/utils/DatePicker.jsx
@@ -6,14 +6,13 @@ import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 
-const DatePicker = ({ handleDateChange }) => {
+const DatePicker = ({ handleDateChange, idx }) => {
   const [value, setValue] = React.useState(dayjs("2023-12-31T23:23:59"));
 
   const handleChange = (newValue) => {
     setValue(newValue);
-    handleDateChange(newValue);
+    handleDateChange(newValue, idx);
   };
-
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack spacing={3}>
