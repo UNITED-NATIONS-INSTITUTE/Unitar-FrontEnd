8bb3a07f0d2eba668d3ba303a751cd2dee7ef548
steve <austines811@gmail.com>
2023-12-01 16:30:44 +0300

    implements basic hackathon renders

--- src/components/organizers/OrgHackathons.jsx
+++ src/components/organizers/OrgHackathons.jsx
@@ -1,41 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Avatars from "../common/Avatars";
 import { useNavigate } from "react-router-dom";
-
+import { getOrganizerHackathons } from "../../api/hackathons/hackathons";
+import { selectOrganizerCode } from "../../features/organizer/organizerSlice";
+import { useSelector } from "react-redux";
 const OrgHackathons = () => {
   const navigate = useNavigate();
-  const images = [
-    { name: "project #1", url: "image1.svg", description: "" },
-    { name: "project #2", url: "image2.png" },
-    { name: "project #3", url: "image3.png" },
-    { name: "project #4", url: "image4.png" },
-  ];
+  const [hackathonsPayload, setHackathonsPalyload] = useState([]);
+  const org_code = useSelector(selectOrganizerCode);
+  const fetchHackathons = () => {
+    getOrganizerHackathons(org_code)
+      .then((res) => {
+        if (res.status === 200) {
+          setHackathonsPalyload(res.data);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    fetchHackathons();
+  }, []);
+
   return (
     <div className="flex space-x-4 mt-5 ml-4">
-      {images.map((image, index) => (
+      {hackathonsPayload.map((field, index) => (
         <div key={index} className="flex flex-col iems-center mb-4">
           <img
             className="rounded-md"
             key={index}
-            src={`/assets/${image.url}`}
-            alt={`image ${index + 1}`}
+            src={field.avatar_url}
+            alt={`Image#${index + 1}`}
             width="250"
             height="200"
           />
-          <p className="mt-2 text-sm text-gray-500">{image.name}</p>
-          <p className="mt-2 text-sm  font-bold">Build4SDGs</p>
-          <p className="mt-2 text-xs text-gray-500">
-            Build4SDGs is a sustainable development startup based in africa
-            working torwards SDGs
-          </p>
+          <p className="mt-2 text-sm  font-bold">{field.title}</p>
+          <p className="mt-2 text-sm text-gray-500">{field.highlight}</p>
+          <p className="mt-2 text-xs text-gray-500">{field.description}</p>
           <div className="flex gap-5">
             <button
               onClick={() => navigate("/organizer/dashboard/detail")}
               className="border border-blue-500 rounded-md text-blue-500 w-[100px] text-xs mt-4 py-1"
             >
-              View project
+              View Details
             </button>
-            <Avatars />
           </div>
         </div>
       ))}
