e82fe3888a683a9b5d17882e2b03e318787b56d0
Isaranwa <pennyisaranwa@gmail.com>
2023-12-02 10:30:52 +0300

    Add the user profile page and style it

--- src/components/organizers/profile/UserProfilePage.jsx
+++ src/components/organizers/profile/UserProfilePage.jsx
@@ -3,42 +3,73 @@ import React from "react";
 
 const UserProfilePage = ({ formData }) => {
   return (
-    <div className="right-side bg-white min-h-screen">
-      <div className="mt-8">
+    <div className="right-side  min-h-screen bg-pattern">
+      <div className=" ml-60">
         <h1 className="text-gray-600 text-[24px] font-bold">User Profile</h1>
-        <div>
-          {" "}
-          <Avatar
-            alt="Profile pic"
-            src={formData.profileImage}
-            sx={{
-              width: "200px",
-              height: "200px",
-              marginTop: "20px",
-              marginBottom: "10px",
-            }}
-          />
-          <button className="bg-custom-blue text-white p-2 rounded-md hover:bg-blue-500 w-[200px] mt-4">
-            Edit Profile
-          </button>
-          <p>
-            <strong>First Name</strong> {formData.firstName}
-          </p>
-          <p className="mt-5">
-            <strong>Last Name</strong> {formData.lastName}
-          </p>
-          <p className="mt-5">
-            <strong>Residence</strong> {formData.residence}
-          </p>
-          <p className="mt-5">
-            <strong>User ID</strong> {formData.userId}
-          </p>
-          <p className="mt-5">
-            <strong>Gender</strong> {formData.gender}
-          </p>
-          <p className="mt-5">
-            <strong>Date of Birth</strong> {formData.dob}
-          </p>
+        <div className="flex flex-row gap-[100px]">
+          <div className=" mt-3 flex justify-center bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+            <div>
+              {" "}
+              <Avatar
+                alt="Profile pic"
+                src={formData.profileImage}
+                sx={{
+                  width: "200px",
+                  height: "200px",
+                  marginTop: "20px",
+                  marginBottom: "10px",
+                }}
+              />
+              <button className="bg-custom-blue text-white p-2 rounded-md hover:bg-blue-500 w-[200px] mt-4">
+                Edit Profile
+              </button>
+            </div>
+          </div>
+          <div className="mt-10">
+            <h1 className="mt-3 mb-2 text-gray-600 font-semibold">
+              Profile Details
+            </h1>
+            <div className="  flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+              <p>
+                <strong className="mr-[100px] ml-[50px] text-custom-blue">
+                  First Name
+                </strong>{" "}
+                {formData.firstName}
+              </p>
+              <p className="mt-5">
+                <strong className="mr-[100px] ml-[50px] text-custom-blue">
+                  Last Name
+                </strong>{" "}
+                {formData.lastName}
+              </p>
+              <p className="mt-5">
+                <strong className="mr-[100px] ml-[50px] text-custom-blue">
+                  Residence
+                </strong>{" "}
+                {formData.residence}
+              </p>
+            </div>
+            <div className=" mt-3 flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+              <p className="mt-5">
+                <strong className="mr-[100px] ml-[50px] text-custom-blue">
+                  User ID
+                </strong>{" "}
+                {formData.userId}
+              </p>
+              <p className="mt-5">
+                <strong className="mr-[100px] ml-[50px] text-custom-blue">
+                  Gender
+                </strong>{" "}
+                {formData.gender}
+              </p>
+              <p className="mt-5">
+                <strong className="mr-[60px] ml-[50px] text-custom-blue">
+                  Date of Birth
+                </strong>{" "}
+                {formData.dob}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
