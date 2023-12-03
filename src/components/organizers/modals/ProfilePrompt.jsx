a30d2415e58506e4cbab846ff1071baee9d5f9bc
steve <austines811@gmail.com>
2023-12-03 13:18:41 +0300

    creates aprofile creation prompt on absence

---
+++ src/components/organizers/modals/ProfilePrompt.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 200,
+  bgcolor: "background.paper",
+  border: "2px solid #089BD9",
+  boxShadow: 24,
+  p: 2,
+  borderRadius: "10px",
+};
+
+export default function ProfilePrompt({ openModal, handleClose }) {
+  const navigate = useNavigate();
+  return (
+    <Box>
+      <Modal
+        open={openModal}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className=" overlay">
+          <Box className=" flex items-center justify-center">
+            <Box>
+              <Typography variant="h6" component="h4" >
+                Complete account set up by creating your profile
+              </Typography>
+              <Box className="flex space-x-4 mt-[100px]">
+                
+                <button
+                  onClick={() => navigate("profile/create")}
+                  className="flex-1 btn-org  py-2 rounded hover:bg-blue-500 text-[16px] "
+                >
+                  Proceed
+                </button>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
+    </Box>
+  );
+}
