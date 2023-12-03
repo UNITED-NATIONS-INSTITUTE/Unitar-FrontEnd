aff0b9bf1925d1a5d1ecc3c1838eadd99696b54d
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-04 02:05:19 +0300

    resized the width of the modal

--- src/components/hackathon/SubscribeModal.jsx
+++ src/components/hackathon/SubscribeModal.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
 
 const style = {
   position: "absolute",
@@ -8,7 +10,7 @@ const style = {
   left: "50%",
   transform: "translate(-50%, -50%)",
   width: 500,
-  height: 250,
+  height: 400,
   bgcolor: "background.paper",
   border: "2px solid #089BD9",
   boxShadow: 24,
@@ -16,7 +18,15 @@ const style = {
   borderRadius: "10px",
 };
 
+// const customStyles = {
+//   fontFamily: "Lexend, sans-serif",
+//   //   marginTop: "20px",
+//   fontSize: "24px",
+// };
+
 export default function SubscribeModal({ openModal, handleClose }) {
+  const navigate = useNavigate();
+
   return (
     <Box>
       <Modal
@@ -35,7 +45,7 @@ export default function SubscribeModal({ openModal, handleClose }) {
                   </h1>
 
                   <p className="mt-12 items-center text-gray-700 text-[20px] ">
-                    You have successfully subscribed to this hackathon project
+                    You have successfully subscribed to this hackathon project🎉
                   </p>
                 </div>
               </Box>
