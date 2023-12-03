8ca43a75859108f8a066261ba2edf4d73601668b
steve <austines811@gmail.com>
2023-12-03 20:40:28 +0300

    adds a slice for hackathon creation steps

--- src/features/hackathon/hackathonSlice.js
+++ src/features/hackathon/hackathonSlice.js
@@ -4,17 +4,23 @@ const hackathonSlice = createSlice({
   name: "hackathon",
   initialState: {
     selectedHackathonDetail: null,
+    currentHackathonDetail: null,
   },
   reducers: {
     setSelectedHackathonDetail: (state, action) => {
       const { selectedHackathonDetail } = action.payload;
       state.selectedHackathonDetail = selectedHackathonDetail;
     },
+    setCurrentHackathonDetail: (state, action) => {
+      const { currentHackathonDetail } = action.payload;
+      state.currentHackathonDetail = currentHackathonDetail;
+    },
   },
 });
 
-export const { setSelectedHackathonDetail } = hackathonSlice.actions;
+export const { setSelectedHackathonDetail, setCurrentHackathonDetail} = hackathonSlice.actions;
 export const selectSelectedHackathonDetail = (state) =>
   state?.hackathon?.selectedHackathonDetail;
-
+  export const selectCurrentHackathonDetail = (state) =>
+  state?.hackathon?.currentHackathonDetail;
 export default hackathonSlice.reducer;
