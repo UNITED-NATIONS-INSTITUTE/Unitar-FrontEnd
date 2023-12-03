78ff165da5b999ffe440b498eb870dd9033e2401
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-04 02:07:55 +0300

    added a set timeout func and rendered the successmessage modal

--- src/components/participants/profile/CreatePartProfile.jsx
+++ src/components/participants/profile/CreatePartProfile.jsx
@@ -3,10 +3,17 @@ import { useSelector } from "react-redux";
 import { Avatar } from "@mui/material";
 import { axiosApi } from "../../../api";
 import { selectLoggedInUserRef } from "../../../features/user/userSlice";
+import SuccessModal from "./SuccessModal";
+import { useNavigate } from "react-router-dom";
 
 const CreatePartProfile = () => {
-  const part_ref = useSelector(selectLoggedInUserRef);
+  const [openSuccessModal, setOpenSuccessModal] = useState(false);
+  const openModal = () => setOpenSuccessModal(true);
+  const closeModal = () => setOpenSuccessModal(false);
+  const navigate = useNavigate();
 
+  const part_ref = useSelector(selectLoggedInUserRef);
+  const [successMessage, setSuccessMessage] = useState(false);
   const [formData, setFormData] = useState({
     first_name: "",
     last_name: "",
@@ -49,122 +56,136 @@ const CreatePartProfile = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     axiosApi
-      .post("/participants", formData) // Assuming the API endpoint is for participants
+      .post("/participants", formData)
       .then((res) => {
         if (res.status === 201) {
-          alert("success");
+          setSuccessMessage(true);
+          openModal();
+
+          // Close the modal after 2000 milliseconds (2 seconds)
+          setTimeout(() => {
+            closeModal();
+            navigate("/participant");
+          }, 2000);
         }
       })
       .catch((err) => {
         console.log(err);
       });
   };
-
   return (
-    <div className="right-side bg-white min-h-screen bg-pattern">
-      <form className=" ml-80 mt-4" onSubmit={handleSubmit}>
-        <h1 className="text-gray-600 text-[24px] font-bold">Profile Details</h1>
-        <div className="w-[500px] h-[140px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
-          <div className="flex items-center ml-4 justify-between">
-            <Avatar
-              alt="Profile pic"
-              src={formData.profileImage}
-              sx={{
-                width: "100px",
-                height: "100px",
-                marginTop: "20px",
-                marginBottom: "50px",
-              }}
-            />
-            <div>
-              <label
-                className=" mr-5  bg-transparent w-[120px]   text-xs border text-custom-blue border-custom-blue  px-2 py-2 rounded-md cursor-pointer"
-                onClick={handleChooseFile}
-              >
-                Add an Image
-                <input type="file" style={{ display: "none" }} />
-              </label>
-            </div>
-          </div>
-        </div>
-        <h1 className="mb-2 text-gray-600 font-semibold">
-          Profile Information here
-        </h1>
-        <div className="grid grid-cols-1 gap-6">
-          <div className="flex flex-row gap-[40px]">
-            <div className=" px-10 py-3 w-[400px] h-[250px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
-              <label className="flex flex-col">
-                <span className="text-gray-700 text-xs mt-[10px]">
-                  First Name
-                </span>
-                <input
-                  type="text"
-                  name="first_name"
-                  value={formData.first_name}
-                  onChange={handleChange}
-                  className="mt-1 mb-2 p-2 w-[300px] border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
-                  required
-                />
-              </label>
-              <label className="flex flex-col">
-                <span className="text-gray-700 text-xs">Last Name</span>
-                <input
-                  type="text"
-                  name="last_name"
-                  value={formData.last_name}
-                  onChange={handleChange}
-                  className="mt-2 mb-2 p-2 w-[300px] border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
-                  required
-                />
-              </label>
+    <div>
+      {successMessage && (
+        <SuccessModal openModal={openModal} handleClose={closeModal} />
+      )}
 
-              <label className="flex flex-col">
-                <span className="text-gray-700 text-xs ">Residence</span>
-                <input
-                  type="text"
-                  name="residence"
-                  value={formData.residence}
-                  onChange={handleChange}
-                  className="mt-2 p-2 w-[300px] bg-inherit border-gray-600 border rounded-md focus:outline-none focus:border-custom-blue"
-                />
-              </label>
-            </div>
-            <div className=" px-10 w-[400px] h-[250px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
-              <label className="flex flex-col">
-                <span className="text-gray-700 text-xs mt-5">Gender</span>
-                <select
-                  name="gender"
-                  value={formData.gender}
-                  onChange={handleChange}
-                  className="mt-1 p-2 w-[300px] mb-8 text-sm border  bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
+      <div className="right-side bg-white min-h-screen bg-pattern">
+        <form className=" ml-80 mt-4" onSubmit={handleSubmit}>
+          <h1 className="text-gray-600 text-[24px] font-bold">
+            Profile Details
+          </h1>
+          <div className="w-[500px] h-[140px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
+            <div className="flex items-center ml-4 justify-between">
+              <Avatar
+                alt="Profile pic"
+                src={formData.profileImage}
+                sx={{
+                  width: "100px",
+                  height: "100px",
+                  marginTop: "20px",
+                  marginBottom: "50px",
+                }}
+              />
+              <div>
+                <label
+                  className=" mr-5  bg-transparent w-[120px]   text-xs border text-custom-blue border-custom-blue  px-2 py-2 rounded-md cursor-pointer"
+                  onClick={handleChooseFile}
                 >
-                  <option value="">Select...</option>
-                  <option value="male">Male</option>
-                  <option value="female">Female</option>
-                </select>
-              </label>
-
-              <label className="flex flex-col">
-                <span className="text-gray-700 text-xs">Date of Birth</span>
-                <input
-                  type="date"
-                  name="date_of_birth"
-                  value={formData.date_of_birth}
-                  onChange={handleChange}
-                  className="mt-1 p-2 w-[300px] text-xs border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
-                />
-              </label>
-
-              <button
-                type="submit"
-                className="bg-custom-blue text-white p-2 rounded-md hover:bg-blue-500 w-[150px] mt-7"
-              >
-                Save Profile
-              </button>
+                  Add an Image
+                  <input type="file" style={{ display: "none" }} />
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-      </form>
+          <h1 className="mb-2 text-gray-600 font-semibold">
+            Profile Information here
+          </h1>
+          <div className="grid grid-cols-1 gap-6">
+            <div className="flex flex-row gap-[40px]">
+              <div className=" px-10 py-3 w-[400px] h-[250px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
+                <label className="flex flex-col">
+                  <span className="text-gray-700 text-xs mt-[10px]">
+                    First Name
+                  </span>
+                  <input
+                    type="text"
+                    name="first_name"
+                    value={formData.first_name}
+                    onChange={handleChange}
+                    className="mt-1 mb-2 p-2 w-[300px] border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
+                    required
+                  />
+                </label>
+                <label className="flex flex-col">
+                  <span className="text-gray-700 text-xs">Last Name</span>
+                  <input
+                    type="text"
+                    name="last_name"
+                    value={formData.last_name}
+                    onChange={handleChange}
+                    className="mt-2 mb-2 p-2 w-[300px] border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
+                    required
+                  />
+                </label>
+
+                <label className="flex flex-col">
+                  <span className="text-gray-700 text-xs ">Residence</span>
+                  <input
+                    type="text"
+                    name="residence"
+                    value={formData.residence}
+                    onChange={handleChange}
+                    className="mt-2 p-2 w-[300px] bg-inherit border-gray-600 border rounded-md focus:outline-none focus:border-custom-blue"
+                  />
+                </label>
+              </div>
+              <div className=" px-10 w-[400px] h-[250px] rounded-md shadow-lg bg-[#f0f6ff] mt-5 mb-5">
+                <label className="flex flex-col">
+                  <span className="text-gray-700 text-xs mt-5">Gender</span>
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleChange}
+                    className="mt-1 p-2 w-[300px] mb-8 text-sm border  bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
+                  >
+                    <option value="">Select...</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </label>
+
+                <label className="flex flex-col">
+                  <span className="text-gray-700 text-xs">Date of Birth</span>
+                  <input
+                    type="date"
+                    name="date_of_birth"
+                    value={formData.date_of_birth}
+                    onChange={handleChange}
+                    className="mt-1 p-2 w-[300px] text-xs border bg-inherit border-gray-600 rounded-md focus:outline-none focus:border-custom-blue"
+                  />
+                </label>
+
+                <button
+                  type="submit"
+                  className="bg-custom-blue text-white p-2 rounded-md hover:bg-blue-500 w-[150px] mt-7"
+                >
+                  Save Profile
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
