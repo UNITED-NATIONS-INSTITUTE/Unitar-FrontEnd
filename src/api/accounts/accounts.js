cd84e43b36481fc5b089a48b5b05ccc77cccaa21
steve <austines811@gmail.com>
2023-12-03 13:17:45 +0300

    adds api endpoints for fetching user profiles

--- src/api/accounts/accounts.js
+++ src/api/accounts/accounts.js
@@ -5,6 +5,9 @@ import { axiosApi } from "..";
 export const getParticipantProfile = async (participant_code) => {
   return await axiosApi.get(`/participants/${participant_code}`);
 };
+export const fetchParticipantProfile = async (user_ref) => {
+  return await axiosApi.get(`/participants/${user_ref}/profile`);
+};
 // implement PATCH profile in form with form data
 export const deleteParticipantProfile = async (participant_code) => {
   return await axiosApi.destroy(`/participants/${participant_code}`);
@@ -14,6 +17,9 @@ export const deleteParticipantProfile = async (participant_code) => {
 // implement create profile in form with form data
 export const getOrganizerProfile = async (organizer_code) => {
   return await axiosApi.get(`/organizers/${organizer_code}`);
+};
+export const fetchOrganizerProfile = async (user_ref) => {
+  return await axiosApi.get(`/organizers/${user_ref}/profile`);
 };
 // implement PATCH profile in form with form data
 export const deleterOganizerProfile = async (organizer_code) => {
