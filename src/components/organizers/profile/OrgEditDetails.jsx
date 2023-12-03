35da2991d718d90d8e8cca3be8f84de8e9096c9f
steve <austines811@gmail.com>
2023-12-03 13:22:01 +0300

    modifies org edit component to hook on redux state for forms prepopulation and intergates apis to update org profile details

--- src/components/organizers/profile/OrgEditDetails.jsx
+++ src/components/organizers/profile/OrgEditDetails.jsx
@@ -1,14 +1,31 @@
-import { Avatar } from "@mui/material";
 import React, { useState } from "react";
-
-import EditInputs from "./EditInputs";
+import { Avatar } from "@mui/material";
 import OrgProfile from "./OrgProfile";
-
+import { useSelector } from "react-redux";
+import { selectCurrentOrganizerDetail } from "../../../features/organizer/organizerSlice";
+import { axiosApi } from "../../../api";
 const OrgEditDetails = () => {
+  const orgProfile = useSelector(selectCurrentOrganizerDetail);
   const [profilePic, setProfilePic] = useState("");
-
-  const handleUpdate = (newPic) => {
-    setProfilePic(newPic);
+  const [formData, setFormData] = useState(orgProfile);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Add logic here to handle form submission
+    try {
+      await axiosApi.patch(`/organizers/${orgProfile.id}`, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+    } catch (error) {
+      console.log(error);
+      alert("Shida!");
+    }
+    // console.log("Form submitted with data:", formData);
   };
 
   const handleDelete = () => {
@@ -16,17 +33,32 @@ const OrgEditDetails = () => {
   };
 
   const handleFileChange = (event) => {
-    const file = event.target.files[0];
-
-    if (file) {
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const newPic = reader.result;
-        handleUpdate(newPic);
-      };
-      reader.readAsDataURL(file);
-    }
+    setProfilePic(event.target.files[0]);
   };
+
+  function handleProfileUpdate() {
+    const imageData = new FormData();
+    imageData.append("profile_image", profilePic);
+    axiosApi.patch(`/organizers/${orgProfile.id}`, imageData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  }
+
+  // const handleFileChange = (event) => {
+  //   // setProfilePic(event.target.files[0])
+  //   const file = event.target.files[0];
+
+  //   if (file) {
+  //     const reader = new FileReader();
+  //     reader.onloadend = () => {
+  //       const newPic = reader.result;
+  //       handleUpdate(newPic);
+  //     };
+  //     reader.readAsDataURL(file);
+  //   }
+  // };
   return (
     <div className="bg-white p-8 right-side min-h-screen">
       <div className="overflow-y-auto  ml-60 flex  profile-details ">
@@ -36,19 +68,65 @@ const OrgEditDetails = () => {
           </h1>
           <span className="text-sm font-semibold">Profile</span>
           <span className="text-sm text-gray-500 mt-2">
-            Update your profile photo and details here
+            Update your details and profile photo here.
           </span>
           <hr className="w-[600px] mt-6" />
-          <EditInputs />
+          <div className="flex flex-col">
+            <form className="flex flex-col" onSubmit={handleSubmit}>
+              <label className="mt-5 mb-2 text-sm text-gray-700">Name</label>
+              <input
+                type="text"
+                name="name"
+                value={formData.name || " "}
+                onChange={handleChange}
+                className="w-[500px] px-3 py-2 border border-gray-400 rounded text-xs
+          focus:outline-none focus:border-custom-blue"
+                placeholder="Google"
+              />
+
+              <label className="mt-5 mb-2 text-sm text-gray-700">
+                Industry
+              </label>
+              <input
+                type="text"
+                name="industry"
+                value={formData.industry || " "}
+                onChange={handleChange}
+                className="w-[500px] px-3 py-2 border border-gray-400 rounded text-xs
+          focus:outline-none focus:border-custom-blue"
+                placeholder="Tech"
+              />
+
+              <label className="mt-5 mb-2 text-sm text-gray-700">
+                Location
+              </label>
+              <input
+                type="text"
+                name="location"
+                value={formData.location || " "}
+                onChange={handleChange}
+                className="w-[500px] px-3 py-2 border border-gray-400 rounded text-xs
+          focus:outline-none focus:border-custom-blue"
+                placeholder="USA"
+              />
+
+              <button
+                type="submit"
+                className="text-white font-semibold bg-custom-blue rounded-md p-2 w-[150px] mt-5"
+              >
+                Update Profile
+              </button>
+            </form>
+          </div>
         </div>
         <div className="flex-2 flex-col flex">
           <div className="flex justify-end mt-0 user-profile">
-            <OrgProfile profilePic={profilePic} />
+            <OrgProfile />
           </div>
 
           <Avatar
             alt="Profile pic"
-            src={profilePic}
+            src={formData.profile_image_url}
             sx={{ width: "100px", height: "100px", marginTop: "50px" }}
           />
           <span className=" text-sm mt-5">Your photo</span>
@@ -56,11 +134,11 @@ const OrgEditDetails = () => {
             This will be displayed on your profile
           </span>
           <div className="flex gap-5 mt-5">
-            <button className="text-red-600 text-xs" onClick={handleDelete}>
+            {/* <button className="text-red-600 text-xs" onClick={handleDelete}>
               Delete
-            </button>
+            </button> */}
             <label className="text-xs text-custom-purple">
-              Update
+              New Profile Image
               <input
                 type="file"
                 accept="image/*"
@@ -68,6 +146,12 @@ const OrgEditDetails = () => {
                 onChange={handleFileChange}
               />
             </label>
+            <button
+              className="text-red-600 text-xs"
+              onClick={handleProfileUpdate}
+            >
+              Update Image
+            </button>
           </div>
         </div>
       </div>
