2d7987a7168af91ecc2ea0acd668e9aa99cbced6
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-04 20:25:26 +0300

    styled the page to make it user friendly

--- src/components/participants/MyHackathons.jsx
+++ src/components/participants/MyHackathons.jsx
@@ -35,7 +35,7 @@ const MyHackathons = () => {
     navigate("/participant/hackathons/submit");
   };
   return (
-    <div className="bg-white right-side min-h-screen  ">
+    <div className="bg-white right-side min-h-screen min-w-full  ">
       <div className=" ml-[280px]">
         <div className="flex justify-between mt-8">
           <h1 className="text-gray-600 font-bold text-[24px]">My Hackathons</h1>
@@ -43,31 +43,33 @@ const MyHackathons = () => {
             <UserProfile />{" "}
           </div>
         </div>
-        <div className="flex space-x-4 mt-5 ml-4">
+        <div className="flex flex-wrap space-x-4 mt-5 ml-4">
           {subscriptionsPayload.length > 0 &&
             subscriptionsPayload.map((field, index) => (
               <div
                 key={index}
-                className="flex flex-col items-center mb-4 w-[400px] h-[350px]"
+                className="flex rounded-[6px] shadow-lg flex-col border border-gray-300   items-center mb-4 w-[400px] h-[350px]"
               >
                 <img
-                  className="rounded-md"
+                  className="rounded-md w-[280px] h-[200px] mt-4"
                   key={index}
                   src={field.hackathon.cover_image_url}
                   alt={`image ${index + 1}`}
-                  width="250"
-                  height="200"
                 />
-                <p className="mt-2 text-sm text-gray-500">{field.hackathon.name}</p>
-                <p className="mt-2 text-sm  font-bold">{field.hackathon.highlight}</p>
-                <p className="mt-2 text-xs text-gray-500">
+                <p className="mt-2 text-sm text-gray-500">
+                  {field.hackathon.name}
+                </p>
+                <p className="mt-2 text-sm  font-bold">
+                  {field.hackathon.highlight}
+                </p>
+                <p className="mt-2 text-[12px] w-[200px] text-center text-gray-500">
                   {field.hackathon.description}
                 </p>
 
                 <div className="flex gap-5">
                   <button
                     onClick={() => handleViewClick(field)}
-                    className="border border-blue-500 rounded-md text-blue-500 w-[150px] text-xs mt-4 py-1"
+                    className="border mb-5 border-blue-500 rounded-md text-blue-500 w-[150px] text-xs mt-4 py-1"
                   >
                     Make a Submission
                   </button>
