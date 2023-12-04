71c1bee99f15003f22e32635177926985f9dce75
Tevin-Isaac <isaactevin@yahoo.com>
2023-12-04 19:36:28 +0300

    updated admin pages

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -1,33 +1,27 @@
-// AdminDashboard.js
-import  { useState } from "react";
+import React, { useState } from "react";
 import logo from "../../assets/unitar-logo.svg";
 import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   const [activePage, setActivePage] = useState("Dashboard");
-
   const handleClickPage = (page) => {
     setActivePage(page);
   };
-
   return (
-    <div className="flex">
-      <div className="left-side bg-light-blue p-4 h-screen fixed left-0 top-0 w-[300px]">
+    <div className=" flex ">
+      <div className=" left-side bg-light-blue p-4 h-screen fixed left-0 top-0 w-[300px] ">
         <div className="flex justify-between">
           <img src={logo} alt="logo" width="100" height="54" />
-          {/* Add a link or button for admin logout or settings if needed */}
-          <Link to="/admin/settings">
-            <img src="/assets/settings.svg" alt="settings" width="20" height="20" />
-          </Link>
+          <img src="/assets/back.svg" alt="back" width="20" height="20" />
         </div>
-        <Link to="/admin/dashboard">
+        <Link to="/participant/dashboard">
           <button
             onClick={() => handleClickPage("Dashboard")}
             style={{
               backgroundColor:
                 activePage === "Dashboard" ? "#089BD9" : "inherit",
             }}
-            className="py-2 px-6 border rounded-md hover:bg-custom-blue mt-16"
+            className="py-2 px-6 border rounded-md hover:bg-custom-blue mt-16  "
           >
             <div className="flex gap-5">
               <img
@@ -40,47 +34,99 @@ const Sidebar = () => {
             </div>
           </button>
         </Link>
-        <Link to="/admin/users">
+        <Link to="/admin/Hackathons">
           <button
-            onClick={() => handleClickPage("Users")}
+            onClick={() => handleClickPage("Hackathons")}
             style={{
               backgroundColor:
-                activePage === "Users" ? "#089BD9" : "inherit",
+                activePage === "Hackathons" ? "#089BD9" : "inherit",
             }}
             className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
           >
             <div className="flex gap-5">
               <img
-                src="/assets/users.svg"
+                src="/assets/hackathon.svg"
                 alt="icon"
                 width="20"
                 height="20"
               />
-              <span>Users</span>
+              <span>Hackathons</span>
             </div>
           </button>
         </Link>
-        <Link to="/admin/reports">
+        <Link to="/admin/Organizers">
           <button
-            onClick={() => handleClickPage("Reports")}
+            onClick={() => handleClickPage("Organizers")}
             style={{
               backgroundColor:
-                activePage === "Reports" ? "#089BD9" : "inherit",
+                activePage === "Organizers" ? "#089BD9" : "inherit",
             }}
             className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
           >
             <div className="flex gap-5">
               <img
-                src="/assets/reports.svg"
+                src="/assets/hackathon.svg"
                 alt="icon"
                 width="20"
                 height="20"
               />
-              <span>Reports</span>
+              <span>Organizers</span>
             </div>
           </button>
         </Link>
-        {/* Add more buttons for other admin features as needed */}
+
+        <Link to="/admin/Participants">
+          <button
+            onClick={() => handleClickPage("Organizers")}
+            style={{
+              backgroundColor:
+                activePage === "Organizers" ? "#089BD9" : "inherit",
+            }}
+            className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
+          >
+            <div className="flex gap-5">
+              <img
+                src="/assets/hackathon.svg"
+                alt="icon"
+                width="20"
+                height="20"
+              />
+              <span>Participants</span>
+            </div>
+          </button>
+        </Link>
+        
+        <Link to="/admin/Organizers">
+          <button
+            onClick={() => handleClickPage("Organizers")}
+            style={{
+              backgroundColor:
+                activePage === "Organizers" ? "#089BD9" : "inherit",
+            }}
+            className="py-2 px-6 border rounded-md mt-5 hover:bg-custom-blue"
+          >
+            <div className="flex gap-5">
+              <img
+                src="/assets/hackathon.svg"
+                alt="icon"
+                width="20"
+                height="20"
+              />
+              <span> Entries</span>
+            </div>
+          </button>
+        </Link>
+
+        <Link>
+          <button
+            onClick={() => handleClickPage("Submission")}
+            style={{
+              backgroundColor:
+                activePage === "Submission" ? "#089BD9" : "inherit",
+            }}
+            className="py-2  px-6 border rounded-md mt-5 hover:bg-custom-blue "
+          ></button>
+        </Link>
       </div>
       <Outlet />
     </div>
