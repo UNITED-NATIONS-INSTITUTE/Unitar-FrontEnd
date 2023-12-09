3685eff5de85fa0fe75abc35ef20eb4ae9497d81
steve <austines811@gmail.com>
2023-12-09 10:24:32 +0300

    adds admin apis

--- src/api/admins/admins.js
+++ src/api/admins/admins.js
@@ -1,13 +1,20 @@
 import { axiosApi } from "..";
 
 export const getAllUsers = async () => {
-  return await axiosApi.get("/users/");
+  return await axiosApi.get("/admins/users/");
+};
+export const activateUserAccount = async (user_code) => {
+  return await axiosApi.get(`admins/users/activate/${user_code}`);
 };
 export const getParticipants = async () => {
-  return await axiosApi.get("/participants/");
+  return await axiosApi.get("/admins/participants/");
 };
 
 export const getOrganizers = async () => {
-  return await axiosApi.get("/organizers/");
+  return await axiosApi.get("/admins/organizers/");
+};
+
+export const getSubmissions = async () => {
+  return await axiosApi.get("/subscriptions");
 };
 // Destroy, statistics, etc....
