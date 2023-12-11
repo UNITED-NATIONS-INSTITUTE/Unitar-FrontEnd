307ee6ffb93c3be0e7f9296fdb2d88ece2d16406
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-11 11:47:36 +0300

    changed the styling of the viewhackdetails

--- src/components/admin/hackathons/ViewHackDetail.jsx
+++ src/components/admin/hackathons/ViewHackDetail.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 // import UserProfile from "../../participants/profile/UserProfile";
+import { useSelector } from "react-redux";
+import moment from "moment";
 
 import HackathonMedia from "../../common/utils/HackathonMedia";
+import { selectSelectedHackathonDetail } from "../../../features/hackathon/hackathonSlice";
 
 const ViewHackDetail = () => {
+  const hackathon = useSelector(selectSelectedHackathonDetail);
+
   return (
     <div className="bg-white p-8  min-h-screen right-side">
       <div className="flex justify-between">
@@ -25,37 +30,80 @@ const ViewHackDetail = () => {
         />
         <span>DevIT</span>
       </p>
-      <div className="flex gap-[100px] ml-60">
-        <div className="flex flex-col">
-          <HackathonMedia
-            cover_image_url="/assets/image2.png"
-            avatar_url="/assets/image3.png"
-          />
-          <div className="flex flex-row gap-10 mt-[30px]">
-            <div className="text-xs">
-              <p className="font-semibold">Location</p>
-              <p className="mt-2">Nairobi</p>
-            </div>
-            <div className="text-xs">
-              <p className="font-semibold">Status</p>
-              <p className="text-green-500 mt-2">Active</p>
-            </div>
+      <div className="ml-60">
+        <div className="flex flex-row gap-[150px]">
+          <div className="relative">
+            {" "}
+            <img
+              src={
+                hackathon.cover_image_url
+                  ? hackathon.cover_image_url
+                  : "/assets/no image (1).jpg"
+              }
+              alt=""
+              style={{
+                borderRadius: "50%",
+                border: "3px solid #089BD9",
+                width: "400px",
+                height: "400px",
+                objectFit: "cover",
+              }}
+            />
+            {/* Style the avatar image */}
+            <img
+              src={
+                hackathon.avatar_url
+                  ? hackathon.avatar_url
+                  : "/assets/no image (1).jpg"
+              }
+              alt=""
+              style={{
+                objectFit: "cover",
+
+                width: "100px",
+                borderRadius: "50%",
+                border: "3px solid #089BD9",
+                height: "100px",
+                left: "200px",
+                bottom: "-10px",
+                position: "absolute",
+              }}
+            />
           </div>
-          <div className="mt-[40px]">
-            <p className="font-semibold text-xs">Timelines</p>
-            <div className="mt-5  gap-5">
-              <div className="mt-5 gap-5">
-                {/* {hackathon.timelines &&
+
+          <div className="flex  items-center flex-1">
+            <h1 className="mt-0 text-custom-blue font-bold  text-[48px]">
+              {hackathon.title}
+            </h1>
+          </div>
+        </div>
+
+        <div className="flex flex-row gap-[200px] mt-10">
+          <div>
+            <div className="flex flex-row gap-10 mt-[30px]">
+              <div className="text-xs">
+                <p className="font-semibold">Location</p>
+                <p className="mt-2">{hackathon.location}</p>
+              </div>
+              <div className="text-xs">
+                <p className="font-semibold">Status</p>
+                <p className="text-green-500 mt-2">{hackathon.status}</p>
+              </div>
+            </div>
+            <div className="mt-[40px]">
+              <p className="font-semibold text-xs">Timelines</p>
+              <div className="mb-5 gap-5">
+                {hackathon.timelines &&
                   hackathon.timelines.map((field, index) => (
-                    <p className="mt-2" key={index}>
+                    <p className="text-xs mt-2" key={index}>
                       {field.period_name}: Start{" "}
-                      {moment(field.start_date).format("Do MMM YYYY ")}
+                      {moment(field.start_date).format("Do MMM YYYY")}
                     </p>
-                  ))} */}
+                  ))}
               </div>
               <p className="font-semibold text-xs">Tags</p>
               <div className="mt-5 flex gap-5">
-                {/* {hackathon.tags &&
+                {hackathon.tags &&
                   hackathon.tags.map((field, index) => (
                     <span
                       key={index}
@@ -63,53 +111,30 @@ const ViewHackDetail = () => {
                     >
                       {field.tag_name}
                     </span>
-                  ))} */}
+                  ))}
               </div>
             </div>
             <div className="flex flex-col text-xs mt-5">
               <p className="font-semibold mt-5 mb-2 ">Prizes</p>
-              <p className="font-semibold ">3800$</p>
+              <p>{hackathon.prize}</p>
             </div>
           </div>
-        </div>
-        <div>
           <div className="flex flex-col w-[500px]">
-            <h1 className="mt-0 text-gray-600 font-bold  text-[20px]">
-              EdTechInnovation{" "}
-            </h1>
-
             <div>
               <p className="text-sm  font-semibold mt-5">Highlights</p>
-              <p className="text-xs mt-5">
-                {" "}
-                Redefining synchronous learning experiences.
-              </p>
+              <p className="text-xs mt-5">{hackathon.highlight}</p>
             </div>
             <div>
               <p className="text-sm  font-semibold mt-5">Description</p>
-              <p className="text-xs mt-5">
-                {" "}
-                A revolutionary platform designed to empower educators with
-                cutting-edge tools for immersive and engaging virtual
-                classrooms.
-              </p>
+              <p className="text-xs mt-5">{hackathon.description}</p>
             </div>
             <div>
               <p className="text-sm  font-semibold mt-5">Deliverables</p>
-              <p className="text-xs mt-5">
-                Pitch Presentation, Prototype/MVP,Demo Video{" "}
-              </p>
+              <p className="text-xs mt-5">{hackathon.deliverables}</p>
             </div>
-            <div>
-              <p className="text-sm font-semibold mt-5">Goals</p>
-              <div className="text-xs">
-                1.Prototype Development: Goal: Create a functional prototype
-                showcasing the core features of the EdTech solution. User
-                2.Engagement: Goal: Collect valuable feedback from judges and
-                potential users to enhance the project. 3.Effective Pitch: Goal:
-                Deliver a compelling pitch presentation highlighting the
-                problem, solution, and impact of the EdTechInnovation.
-              </div>
+            <div className="text-xs ">
+              <p className=" font-semibold mt-5">Goals</p>
+              <p className="mt-2">{hackathon.goals}</p>
             </div>
           </div>
         </div>
