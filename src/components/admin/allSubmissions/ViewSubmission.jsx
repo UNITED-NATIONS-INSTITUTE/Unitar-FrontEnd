74c6786c56641e56797264255c5fe5734223ab02
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-13 15:52:42 +0300

    added an admin profile and also fetch submission detail data and displayed it

--- src/components/admin/allSubmissions/ViewSubmission.jsx
+++ src/components/admin/allSubmissions/ViewSubmission.jsx
@@ -1,6 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import AdminProfile from "../AdminProfile";
+import { getSubmissions } from "../../../api/admins/admins";
 
 const ViewSubmissions = () => {
+  const [submissionsPayload, setSubmissionsPayload] = useState([]);
+  const fetchSubmissions = () => {
+    getSubmissions().then((res) => {
+      if (res.status === 200) {
+        setSubmissionsPayload(res.data);
+        console.log(res.data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchSubmissions();
+  }, []);
   return (
     <div className="bg-white right-side min-h-screen min-w-full">
       <div className="ml-[280px]">
@@ -8,24 +23,96 @@ const ViewSubmissions = () => {
           <h1 className="text-gray-600 font-bold text-[24px]">
             Submitted Hackathons
           </h1>
-          <div className="mr-10">{/* <UserProfile /> */}</div>
-        </div>
-        <div className="flex flex-wrap space-x-4 mt-5 ml-4">
-          <div className="flex rounded-[6px] shadow-xl flex-col border border-gray-100  mb-4 w-[300px] h-[400px] transition-transform transform hover:-translate-y-1">
-            <img
-              className="rounded-md w-[300px] h-[200px]"
-              src="/assets/image2.png"
-              alt="image"
-            />
-            <p className="mt-2 text-sm ml-4 text-gray-500">EdTechInnovation </p>
-            <p className="mt-2 text-sm ml-4 font-bold">
-              Redefining synchronous learning experiences.
-            </p>
-            <p className="mt-2 text-[12px] ml-4 w-[200px]  text-gray-500">
-              A revolutionary platform designed to empower educators with
-              cutting-edge tools for immersive and engaging virtual classrooms.{" "}
-            </p>
+          <div className="mr-10">
+            <AdminProfile />
           </div>
+        </div>
+        <div className="flex flex-wrap  space-x-4 mt-5 ml-4">
+          {submissionsPayload.map((submission) => (
+            <div
+              key={submission.id}
+              className="flex gap-[100px] rounded-[6px] shadow flex-row border border-gray-100  mb-4  h-full w-full"
+            >
+              {/* Image on the right */}
+              <div className="flex-shrink-0 rounded-full mt-3 ml-3 mb-3">
+                <img
+                  src={submission.hackathon.cover_image_url}
+                  alt=""
+                  style={{
+                    borderRadius: "50%",
+                    border: "3px solid #089BD9",
+                    width: "400px",
+                    height: "400px",
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+              <div className="ml-4 mt-5">
+                <p className="mt-3 text-custom-blue">
+                  Blog:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {" "}
+                    {submission.blog}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Demo Link:
+                  <span className="text-sm text-black ml-3">
+                    {submission.demo_link}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Description:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.desc}
+                  </span>
+                </p>
+
+                <p className="mt-3 text-custom-blue">
+                  Live URL:
+                  <span className="text-sm text-black ml-3">
+                    {submission.live_url}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Description:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.desc}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Grade:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.grade}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  GitHub Link:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.gh_link}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Prize:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.hackathon.prize}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Status:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.hackathon.status}
+                  </span>
+                </p>
+                <p className="mt-3 text-custom-blue">
+                  Location:{" "}
+                  <span className="text-sm text-black ml-3">
+                    {submission.hackathon.location}
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
