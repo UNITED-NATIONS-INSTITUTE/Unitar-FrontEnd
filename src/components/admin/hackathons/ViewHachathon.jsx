ea163f08bea968fb036021302e5aff50a7a135d6
steve <austines811@gmail.com>
2023-12-18 18:29:07 +0300

    obtain from store to display

--- src/components/admin/hackathons/ViewHachathon.jsx
+++ src/components/admin/hackathons/ViewHachathon.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import HackathonMedia from "../../common/utils/HackathonMedia";
 import AdminProfile from "../AdminLogOut";
-
+import { selectSelectedHackathonDetail } from "../../../features/hackathon/hackathonSlice";
 const ViewHackathon = () => {
   const navigate = useNavigate();
+  const hackathonDetails = useSelector(selectSelectedHackathonDetail);
   return (
     <div className="bg-white right-side min-h-screen ">
       <div className="ml-[300px]">
@@ -21,19 +23,19 @@ const ViewHackathon = () => {
         <div className="flex flex-wrap space-x-4 mt-5 ml-4">
           <div className="hover:border-custom-blue relative overflow-hidden border border-[#C7C7C7]  rounded-[20px] shadow mb-4 w-[300px] h-[380px] transition-transform transform hover:-translate-y-1">
             <HackathonMedia
-              cover_image_url="/assets/image2.png"
-              avatar_url="/assets/image3.png"
+              cover_image_url={hackathonDetails.cover_image_url}
+              avatar_url={hackathonDetails.avatar_url}
             />
             <div className="relative">
               <div className="absolute bottom-0 left-0 border-[#7C7C7C] border-t right-0 h-1/3 bg-white p-4 rounded-[20px]">
-                <p className="text-sm font-bold mt-4">EdTechInnovation</p>
+                <p className="text-sm font-bold mt-4">
+                  {hackathonDetails.title}
+                </p>
                 <p className="text-sm text-gray-700">
-                  Redefining synchronous learning experiences.
+                  {hackathonDetails.highlight}{" "}
                 </p>
                 <p className="text-xs text-gray-500  mt-2 w-[250px] h-[50px]">
-                  A revolutionary platform designed to empower educators with
-                  cutting-edge tools for immersive and engaging virtual
-                  classrooms.
+                  {hackathonDetails.description}
                 </p>
               </div>
             </div>
