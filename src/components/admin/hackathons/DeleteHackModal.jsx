3e83f4b7a79e8a94074ba2bb6700d89b12fd489e
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-19 21:38:31 +0300

    added a new state for handling the error and implemented the modal for success deletion

--- src/components/admin/hackathons/DeleteHackModal.jsx
+++ src/components/admin/hackathons/DeleteHackModal.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import { useNavigate } from "react-router-dom";
 import { selectSelectedHackathonDetail } from "../../../features/hackathon/hackathonSlice";
 import { deleteHackathon } from "../../../api/hackathons/hackathons";
+
 const style = {
   position: "absolute",
   top: "50%",
@@ -19,63 +20,82 @@ const style = {
 };
 
 export default function DeleteHackModal({ openModal, closeModal }) {
-  const hackathon = useSelector(selectSelectedHackathonDetail)
-  const hackathon_code = hackathon.id
+  const hackathon = useSelector(selectSelectedHackathonDetail);
+  const hackathon_code = hackathon.id;
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [error, setError] = useState(null); // New state for error
 
-  function handleDelete (id){
+  const navigate = useNavigate();
+
+  function handleDelete(id) {
     deleteHackathon(id).then((res) => {
-      if (res.status === 204){
-        alert("Hackathon entry deleted")
-      }else {
-        alert ("error removing entry")
+      if (res.status === 204) {
+        setModalOpen(true);
+        setTimeout(() => {
+          closeModal();
+          navigate("/participants");
+        }, 3000);
+      } else {
+        setError("Error removing entry");
       }
-    })
+    });
   }
-  return (
-    <Box>
-      <Modal
-        open={openModal}
-        onClose={closeModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Box className=" flex items-center justify-center">
-            <Box>
-              <Box className="flex space-x-4 ">
-                <div>
-                  <h1 className="font-bold text-[20px]  font-Lexend-Exa  text-center">
-                    Delete Hackathon
-                  </h1>
-                  <div className="flex justify-center ">
-                    <img
-                      src="/assets/bin.jpg"
-                      alt=""
-                      className="w-[80px] h-[80px]"
-                    />
-                  </div>
-                  <p className=" text-center text-gray-700 text-sm ">
-                    Do you really want to delete this Hackathon? Deleting this
-                    Hackathon will erase all their data
-                  </p>
 
-                  <div className="flex flex-row gap-5 mt-5 justify-center">
-                    <button onClick={() => handleDelete(hackathon_code)} className="bg-[#D40C0C] text-white font-bold w-[150px] py-2 px-2 rounded-md ">
-                      Yes, Delete
-                    </button>
-                    <button
-                      onClick={closeModal}
-                      className="py-2 border border-black rounded-md w-[150px]"
-                    >
-                      No, Cancel
-                    </button>
-                  </div>
-                </div>
+  return (
+    <>
+      {isModalOpen && (
+        <Box>
+          <Modal
+            open={openModal}
+            onClose={closeModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Box className="flex items-center justify-center">
+                <Box>
+                  <Box className="flex space-x-4 ">
+                    <div>
+                      <h1 className="font-bold text-[20px] font-Lexend-Exa text-center">
+                        Delete Hackathon
+                      </h1>
+                      <div className="flex justify-center ">
+                        <img
+                          src="/assets/bin.jpg"
+                          alt=""
+                          className="w-[80px] h-[80px]"
+                        />
+                      </div>
+                      <p className="text-center text-gray-700 text-sm ">
+                        {error ? (
+                          <span className="text-red-500">{error}</span>
+                        ) : (
+                          "Do you really want to delete this Hackathon? Deleting this Hackathon will erase all their data"
+                        )}
+                      </p>
+
+                      <div className="flex flex-row gap-5 mt-5 justify-center">
+                        <button
+                          onClick={() => handleDelete(hackathon_code)}
+                          className="bg-[#D40C0C] text-white font-bold w-[150px] py-2 px-2 rounded-md "
+                        >
+                          Yes, Delete
+                        </button>
+                        <button
+                          onClick={closeModal}
+                          className="py-2 border border-black rounded-md w-[150px]"
+                        >
+                          No, Cancel
+                        </button>
+                      </div>
+                    </div>
+                  </Box>
+                </Box>
               </Box>
             </Box>
-          </Box>
+          </Modal>
         </Box>
-      </Modal>
-    </Box>
+      )}
+    </>
   );
 }
