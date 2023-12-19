e8b89c05313c204dd8ee4f54f80f478b8fd9aca9
steve <austines811@gmail.com>
2023-12-19 12:21:07 +0300

    code clean up to remove unused lines

--- src/components/admin/charts/HackathonCharts.jsx
+++ src/components/admin/charts/HackathonCharts.jsx
@@ -49,26 +49,3 @@ const HackathonChart = () => {
 };
 
 export default HackathonChart;
-// <LineChart
-//     width={700}
-//     height={300}
-//     data={hackathons}
-//     margin={{
-//       top: 5,
-//       right: 30,
-//       left: 20,
-//       bottom: 5,
-//     }}
-//   >
-//     <CartesianGrid strokeDasharray="3 3" />
-//     <XAxis dataKey="name" />
-//     <YAxis />
-//     <Tooltip />
-//     <Legend />
-//     <Line
-//       type="monotone"
-//       dataKey="sub_count"
-//       stroke="#089BD9"
-//       activeDot={{ r: 8 }}
-//     />
-//   </LineChart>
