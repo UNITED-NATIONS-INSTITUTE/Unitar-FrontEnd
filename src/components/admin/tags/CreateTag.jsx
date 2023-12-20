b2824b862186f62d8f4746a382c65d8063d7726d
steve <austines811@gmail.com>
2023-12-20 14:07:27 +0300

    refactors tag creation component for creation actions

--- src/components/admin/tags/CreateTag.jsx
+++ src/components/admin/tags/CreateTag.jsx
@@ -1,30 +1,53 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectLoggedInUserRef } from "../../../features/user/userSlice";
+import { createTag } from "../../../api/hackathons/hackathons";
 const CreateTag = () => {
+  const [values, setValues] = useState({
+    category: "",
+  });
+  const { category } = values;
+  const created_by = useSelector(selectLoggedInUserRef);
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createTag(category, created_by).then((res) => {
+      // TO DO: Handle navigation and modal infomatics here
+      if (res.status === 201) {
+        alert("category created");
+      } else {
+        alert("Error!");
+      }
+    });
+  };
   return (
-    <div className="min-h-screen bg-white right-side">
-      <div className=" flex  items-center justify-center mt-10  ">
-        <div className="ml-60">
-          <div className="w-[600px] h-[400px] rounded-lg shadow-md  items-center border border-gray-100">
-            <h1 className=" text-[28px] text-custom-blue text-center font-bold mt-6">
-              Create a Tag
-            </h1>
-            <form className="text-center mt-10">
-              <label className="block text-sm ">Enter name</label>
+    // TO DO: CHECK ON STYLES AND DISPLAYS
+    <div className=" flex">
+      <div className="ml-60">
+        <div className="w-[600px] h-[400px] rounded-lg shadow-md  items-center border border-gray-100">
+          <h1 className=" text-[20px] text-center font-bold mt-6">
+            Create a new hackathon category
+          </h1>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="m-4">
+              <label className="block text-md m-2">Category Name</label>
               <input
-                type="email"
-                className="w-[200px] px-3 py-2 border border-grey-600 rounded text-xs mb-2"
-                placeholder="finance"
-              />{" "}
-              <br />
+                type="text"
+                className="w-full px-3 py-2 border border-grey-600 rounded text-xs"
+                placeholder="Financial Inclusion"
+                value={category}
+                onChange={handleChange("category")}
+              />
               <button
                 type="submit"
-                className="mt-[100px] bg-custom-blue text-white py-2 px-8 rounded hover:bg-white hover:text-custom-blue hover:border-2 hover:border-custom-blue "
+                className="w-full bg-custom-blue text-white py-2 mt-4 rounded hover:bg-white hover:text-custom-blue hover:border-2 hover:border-custom-blue "
               >
-                Create a Tag
+                Create Category
               </button>
-            </form>
-          </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
