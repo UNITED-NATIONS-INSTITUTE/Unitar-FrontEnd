6305d77c881fb1a8019b986687ada29344680e63
Isaranwa <pennyisaranwa@gmail.com>
2023-12-22 01:04:59 +0300

    Added a successful activation modal

--- src/components/admin/user/ActivateModal.jsx
+++ src/components/admin/user/ActivateModal.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
 import { activateUserAccount } from "../../../api/admins/admins";
+import SuccessfulActivationModal from "./SuccessfulActivation";
 const style = {
   position: "absolute",
   top: "50%",
@@ -16,63 +17,75 @@ const style = {
 };
 
 export default function ActivateModal({ openModal, closeModal, user_id }) {
+  const [isActivateModalOpen, setActivateModalOpen] = useState(false);
   const adminActivateUser = () => {
     activateUserAccount(user_id).then((res) => {
       if (res.status === 200) {
-        // raise success message and close the activate modal
         closeModal();
+        setActivateModalOpen(true);
+        setTimeout(() => {
+          setActivateModalOpen(false);
+          window.location.reload();
+        }, 2000);
       } else {
-        // raise error message
         console.log(res.data);
       }
     });
   };
   return (
-    <Box>
-      <Modal
-        open={openModal}
-        onClose={closeModal}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Box className=" flex items-center justify-center">
-            <Box>
-              <Box className="flex space-x-4 ">
-                <div>
-                  <h1 className="font-bold text-[20px]  font-Lexend-Exa  text-center">
-                    Activate User's Account
-                  </h1>
-                  <div className="flex justify-center ">
-                    <img
-                      src="/assets/activate.jpg"
-                      alt=""
-                      className="w-[100px] h-[100px]"
-                    />
+    <>
+      {isActivateModalOpen && (
+        <SuccessfulActivationModal
+          CloseModal={() => setActivateModalOpen(false)}
+          openModal={isActivateModalOpen}
+        />
+      )}
+      <Box>
+        <Modal
+          open={openModal}
+          onClose={closeModal}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Box className=" flex items-center justify-center">
+              <Box>
+                <Box className="flex space-x-4 ">
+                  <div>
+                    <h1 className="font-bold text-[20px]  font-Lexend-Exa  text-center">
+                      Activate User's Account
+                    </h1>
+                    <div className="flex justify-center ">
+                      <img
+                        src="/assets/activate.jpg"
+                        alt=""
+                        className="w-[100px] h-[100px]"
+                      />
+                    </div>
+                    <p className=" text-center text-gray-700 text-sm mt-2 ">
+                      Do you really want to activate this account?
+                    </p>
+                    <div className="flex flex-row gap-5 mt-10">
+                      <button
+                        onClick={() => adminActivateUser()}
+                        className="bg-[#06BD0E] text-white font-bold w-[150px] py-2 px-2 rounded-md "
+                      >
+                        Yes, Activate
+                      </button>
+                      <button
+                        onClick={closeModal}
+                        className="border  border-black rounded-md w-[150px] py-2 px-2  "
+                      >
+                        No, Cancel
+                      </button>
+                    </div>
                   </div>
-                  <p className=" text-center text-gray-700 text-sm mt-2 ">
-                    Do you really want to activate this account?
-                  </p>
-                  <div className="flex flex-row gap-5 mt-10">
-                    <button
-                      onClick={() => adminActivateUser()}
-                      className="bg-[#06BD0E] text-white font-bold w-[150px] py-2 px-2 rounded-md "
-                    >
-                      Yes, Activate
-                    </button>
-                    <button
-                      onClick={closeModal}
-                      className="border  border-black rounded-md w-[150px] py-2 px-2  "
-                    >
-                      No, Cancel
-                    </button>
-                  </div>
-                </div>
+                </Box>
               </Box>
             </Box>
           </Box>
-        </Box>
-      </Modal>
-    </Box>
+        </Modal>
+      </Box>
+    </>
   );
 }
