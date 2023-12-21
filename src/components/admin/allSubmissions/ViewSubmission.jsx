fe6f432d29ea12b5181abdd6ebf262561a5b74c0
Isaranwa <pennyisaranwa@gmail.com>
2023-12-21 18:28:48 +0300

    Made the links to be href tags

--- src/components/admin/allSubmissions/ViewSubmission.jsx
+++ src/components/admin/allSubmissions/ViewSubmission.jsx
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { selectCurrentSubscriptionDetail } from "../../../features/subscription/subscriptionSlice";
 
 const ViewSubmissions = () => {
-  const submissionDetails = useSelector(selectCurrentSubscriptionDetail)
+  const submissionDetails = useSelector(selectCurrentSubscriptionDetail);
 
   return (
     <div className="bg-white right-side min-h-screen min-w-full">
@@ -21,7 +21,7 @@ const ViewSubmissions = () => {
           {submissionDetails && (
             <div
               key={submissionDetails.id}
-              className="flex gap-[100px] rounded-[6px] shadow flex-row border border-gray-100  mb-4  h-full w-full"
+              className="flex gap-[100px] rounded-[6px] shadow flex-row border border-gray-100  p-10 h-full w-full "
             >
               <div className="flex-shrink-0 rounded-full mt-3 ml-3 mb-3">
                 <img
@@ -37,64 +37,90 @@ const ViewSubmissions = () => {
               </div>
               <div className="ml-4 mt-5">
                 <p className="mt-3 ">
-                  Blog:{" "}
-                  <span className="text-sm text-black ml-3">
-                    {" "}
-                    {submissionDetails.blog}
+                  Project Name:
+                  <span className="text-sm text-gray-600 ml-3">
+                    {submissionDetails.title}
                   </span>
+                </p>
+
+                <p className="mt-3 ">
+                  Blog:{" "}
+                  <a
+                    href={submissionDetails.blog}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm text-blue-500 ml-3"
+                  >
+                    {submissionDetails.blog}
+                  </a>
                 </p>
                 <p className="mt-3 ">
                   Demo Link:
-                  <span className="text-sm text-black ml-3">
+                  <a
+                    href={submissionDetails.demo_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm text-blue-500 ml-3"
+                  >
                     {submissionDetails.demo_link}
-                  </span>
+                  </a>
                 </p>
                 <p className="mt-3 ">
                   Description:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3">
                     {submissionDetails.desc}
                   </span>
                 </p>
 
                 <p className="mt-3">
                   Live URL:
-                  <span className="text-sm text-black ml-3">
+                  <a
+                    href={submissionDetails.live_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm text-blue-500 ml-3"
+                  >
                     {submissionDetails.live_url}
-                  </span>
+                  </a>
                 </p>
                 <p className="mt-3 ">
                   Description:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3">
                     {submissionDetails.desc}
                   </span>
                 </p>
                 <p className="mt-3 ">
                   Grade:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3">
                     {submissionDetails.grade}
                   </span>
                 </p>
                 <p className="mt-3 ">
                   GitHub Link:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <a
+                    href={submissionDetails.gh_link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-sm text-blue-500 ml-3"
+                  >
                     {submissionDetails.gh_link}
-                  </span>
+                  </a>
                 </p>
                 <p className="mt-3">
                   Prize:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3">
                     {submissionDetails.hackathon.prize}
                   </span>
                 </p>
                 <p className="mt-3 ">
                   Status:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3">
                     {submissionDetails.hackathon.status}
                   </span>
                 </p>
                 <p className="mt-3 ">
                   Location:{" "}
-                  <span className="text-sm text-black ml-3">
+                  <span className="text-sm text-gray-600 ml-3 ">
                     {submissionDetails.hackathon.location}
                   </span>
                 </p>
