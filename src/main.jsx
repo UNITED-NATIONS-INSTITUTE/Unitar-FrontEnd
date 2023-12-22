f9cfba34abc137387a27992faf8f674dc34b1bdb
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-22 23:32:24 +0300

    removed the index.css

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import { persistor, store } from "./store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "@mui/material/styles";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
