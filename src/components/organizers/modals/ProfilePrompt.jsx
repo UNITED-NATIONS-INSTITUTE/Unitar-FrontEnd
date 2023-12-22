04ba01fb8e364d281974565a32a60d310ba5e069
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-23 00:48:24 +0300

    made the modal responsive on all devices

--- src/components/organizers/modals/ProfilePrompt.jsx
+++ src/components/organizers/modals/ProfilePrompt.jsx
@@ -27,16 +27,33 @@ export default function ProfilePrompt({ openModal, handleClose }) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box
+          sx={{
+            ...style,
+            "@media (max-width: 600px)": {
+              width: 900,
+              height: 500,
+            },
+          }}
+        >
           <Box className=" flex items-center justify-center">
             <Box>
-              <Typography variant="h6" component="h4">
+              <Typography
+                variant="h6"
+                component="h4"
+                sx={{
+                  "@media (max-width: 600px)": {
+                    fontSize: "40px",
+                    marginLeft: "28px",
+                  },
+                }}
+              >
                 Complete account set up by creating your profile
               </Typography>
               <Box className="flex space-x-4  mt-[40px]">
                 <button
                   onClick={() => navigate("profile/create")}
-                  className="flex-1 bg-custom-blue rounded-md  py-2 text-white hover:bg-blue-500 text-[16px] "
+                  className=" bg-custom-blue rounded-md lg:w-full sm:ml-[68px] ml-[280px] md:ml-3 lg:ml-1  w-[300px] text-center  py-6 md:py-1 lg:py-2 text-[30px] text-white hover:bg-blue-500 lg:text-[16px] "
                 >
                   Proceed
                 </button>
