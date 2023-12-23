914ffa593ee67fe4c99dc8dc3bd62805a1fb6a0c
Isaranwa <pennyisaranwa@gmail.com>
2023-12-23 13:44:04 +0300

    Add linear progress for loading data

--- src/components/organizers/OrgSubmissionPage.jsx
+++ src/components/organizers/OrgSubmissionPage.jsx
@@ -7,7 +7,10 @@ import { selectOrganizerCode } from "../../features/organizer/organizerSlice";
 import { setSelectedHackathonDetail } from "../../features/hackathon/hackathonSlice";
 import { getOrganizerHackathons } from "../../api/hackathons/hackathons";
 import HackathonMedia from "../common/utils/HackathonMedia";
+import { LinearProgress } from "@mui/material";
+
 const OrgSubmissionPage = () => {
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [hackathonsPayload, setHackathonsPalyload] = useState([]);
@@ -17,6 +20,7 @@ const OrgSubmissionPage = () => {
       .then((res) => {
         if (res.status === 200) {
           setHackathonsPalyload(res.data);
+          setLoading(false);
         }
       })
       .catch((err) => {
@@ -42,37 +46,40 @@ const OrgSubmissionPage = () => {
           <OrgProfile />
         </div>
         <p className="text-gray-600 text-sm font-semibold">Our Hackathons</p>
-        <div className="flex flex-wrap space-x-4 mt-5 ml-4">
-          {hackathonsPayload.length > 0 &&
-            hackathonsPayload.map((field, index) => (
-              <div
-                key={index}
-                className=" hover:border-custom-blue relative overflow-hidden border border-[#C7C7C7]  rounded-[20px] shadow mb-4 w-[300px] h-[380px] transition-transform transform hover:-translate-y-1"
-              >
-                <HackathonMedia
-                  cover_image_url={field.cover_image_url}
-                  avatar_url={field.avatar_url}
-                />
-                <div className="relative">
-                  <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-white p-4 border-[#7C7C7C] border-t rounded-[20px]">
-                    <p className="text-sm font-bold mt-4">{field.title}</p>
-                    <p className="text-sm text-gray-700">{field.highlight}</p>
-                    <p className="text-xs text-gray-500  mt-2 h-[45px] overflow-hidden">
-                      {field.description}
-                    </p>{" "}
+        {loading && <LinearProgress />}
+        {!loading && (
+          <div className="flex flex-wrap space-x-4 mt-5 ml-4">
+            {hackathonsPayload.length > 0 &&
+              hackathonsPayload.map((field, index) => (
+                <div
+                  key={index}
+                  className=" hover:border-custom-blue relative overflow-hidden border border-[#C7C7C7]  rounded-[20px] shadow mb-4 w-[300px] h-[380px] transition-transform transform hover:-translate-y-1"
+                >
+                  <HackathonMedia
+                    cover_image_url={field.cover_image_url}
+                    avatar_url={field.avatar_url}
+                  />
+                  <div className="relative">
+                    <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-white p-4 border-[#7C7C7C] border-t rounded-[20px]">
+                      <p className="text-sm font-bold mt-4">{field.title}</p>
+                      <p className="text-sm text-gray-700">{field.highlight}</p>
+                      <p className="text-xs text-gray-500  mt-2 h-[30px] overflow-hidden">
+                        {field.description}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex gap-5 mt-[110px] ml-[22px]">
+                    <button
+                      onClick={() => handleViewClick(field)}
+                      className="border border-blue-500 rounded-md text-blue-500 w-[250px] text-xs mt-4 py-2 hover:bg-custom-blue hover:text-white"
+                    >
+                      View submissions
+                    </button>{" "}
                   </div>
                 </div>
-                <div className="flex gap-5 mt-[110px] ml-[22px]">
-                  <button
-                    onClick={() => handleViewClick(field)}
-                    className="border border-blue-500 rounded-md text-blue-500 w-[250px] text-xs mt-4 py-2 hover:bg-custom-blue hover:text-white"
-                  >
-                    View submissions
-                  </button>{" "}
-                </div>
-              </div>
-            ))}
-        </div>
+              ))}
+          </div>
+        )}
       </div>
     </div>
   );
