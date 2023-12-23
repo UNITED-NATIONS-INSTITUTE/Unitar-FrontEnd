7eda94de73e71cdb437908082fdfcc68209441ba
Isaranwa <pennyisaranwa@gmail.com>
2023-12-23 13:03:00 +0300

    Adjust the size of the logo image

--- src/components/participants/Sidebar.jsx
+++ src/components/participants/Sidebar.jsx
@@ -20,8 +20,8 @@ const Sidebar = () => {
     <div className=" flex ">
       <div className="  bg-light-blue p-4 h-screen fixed left-0 top-0 w-[250px] ">
         <div className="flex justify-between">
-          <img src={logo} alt="logo" width="100" height="54" />
-          <img src="/assets/back.svg" alt="back" width="20" height="20" />
+          <img src={logo} alt="" />
+          {/* <img src="/assets/back.svg" alt="back" width="20" height="20" /> */}
         </div>
         <button
           onClick={() => navigate("/participant/dashboard")}
