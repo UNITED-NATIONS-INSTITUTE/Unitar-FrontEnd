0e7166d8efc7c20b4d5f5c684506fd82359847c5
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-30 02:53:49 +0300

    added some media query breakpoints to ensure responsiveness

--- src/components/hackathon/hackathonDashboard/HackathonDashboard.jsx
+++ src/components/hackathon/hackathonDashboard/HackathonDashboard.jsx
@@ -6,13 +6,13 @@ import OpenHackathon from "./OpenHackathon";
 const HackathonDashboard = () => {
   return (
     <>
-      <div className="overflow-y-auto  ml-60 bg-white p-8 right-side min-h-screen">
+      <div className="overflow-y-auto  md:ml-60 bg-white p-8 right-side min-h-screen">
         <div className="flex justify-between">
           <h1 className="text-gray-600 font-bold text-[24px]">Hackathons</h1>
           <UserProfile />
         </div>
-        <div className="mt-10">
-          <h1 className="text-gray-600 font-semibold text-sm">
+        <div className="md:mt-10 mt-5">
+          <h1 className="text-gray-600 font-semibold md:text-sm text-[18px]">
             Open Hackathons
           </h1>
           <OpenHackathon />
