3bdf6afcd29764da32ac962c6a313a21ee0f7670
Isaranwa <pennyisaranwa@gmail.com>
2023-12-29 22:54:42 +0300

    Refactor the profile name

--- src/components/participants/profile/UserProfile.jsx
+++ src/components/participants/profile/UserProfile.jsx
@@ -63,7 +63,7 @@ const UserProfile = () => {
               src={partProfile.profile_image_url}
               sx={{ width: "24px", height: "24px" }}
             />
-            <span className="text-xs">{partProfile.first_name}</span>
+            <span className="text-xs">{partProfile.full_name}</span>
           </div>
         ) : null}
         <Menu
