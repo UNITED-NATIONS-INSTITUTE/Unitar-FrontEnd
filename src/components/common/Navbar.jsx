e105b87b9804ce554d112e020926bad352c74509
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-30 02:52:56 +0300

    added some useState and useEffect logix to handle the navbg and navshadow when user scrolls

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,16 +1,49 @@
-import React, { useState } from "react";
-import logo from "../../assets/unitar-logo.svg";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import BasicModal from "./SignUpModal";
 import { close, menu } from "../../assets";
-import { FiMenu } from "react-icons/fi";
+import logo from "../../assets/unitar-logo.svg";
+
 const Navbar = ({ openModal }) => {
+  const [activePage, setActivePage] = useState("Dashboard");
+  const pathnameArray = location.pathname.split("/");
+
   const navigate = useNavigate();
+  useEffect(() => {
+    if (pathnameArray[2] === undefined || pathnameArray[2] === "dashboard") {
+      setActivePage("dashboard");
+    } else if (pathnameArray[2] === "hackathons") {
+      setActivePage("hackathons");
+    } else if (pathnameArray[2] === "myhackathons") {
+      setActivePage("myhackathons");
+    }
+  }, [pathnameArray]);
   const [toggle, setToggle] = useState(false);
+  const [shadow, setShadow] = useState(false);
+  const [navBg, setNavBg] = useState("#E2EDF1");
+  useEffect(() => {
+    const handleShadow = () => {
+      if (window.scrollY >= 90) {
+        setShadow(true);
+        setNavBg("#E2EDF1");
+      } else {
+        setShadow(false);
+        setNavBg("transparent");
+      }
+    };
+    window.addEventListener("scroll", handleShadow);
+  }, []);
 
   return (
     <nav>
-      <div className="w-full flex py-6 justify-between items-center navbar">
+      <div
+        style={{ backgroundColor: `${navBg}` }}
+        className={
+          shadow
+            ? "w-full fixed z-[1000]  py-6 justify-between items-center flex shadow-xl ease-in-out duration-300"
+            : "w-full fixed z-[1000]  py-6 justify-between items-center flex"
+        }
+      >
         <img
           src={logo}
           alt="logo"
@@ -44,22 +77,21 @@ const Navbar = ({ openModal }) => {
       <div
         className={`${
           !toggle ? "hidden" : "flex"
-        } p-12  md:hidden fixed  top-20 right-0 mx-4 my-2  min-w-[140px] rounded-xl  w-full h-screen `}
+        } p-12  md:hidden fixed  top-20 right-0 mx-4 my-2 bg-black/70 z-[1000] rounded-xl  w-full h-screen `}
       >
-        <div className="z-20">
+        <div className="ml-10">
           {" "}
           <button
             onClick={() => navigate("/login")}
-            className=" rounded-[8px]  mr-4 border border-custom-blue  p-2 h-50 text-custom-blue text-[18px] transition-transform transform hover:-translate-y-1 "
+            className=" rounded-[8px] w-full  mr-4 border bg-custom-blue  p-2 h-50 text-white text-[22px] transition-transform transform hover:-translate-y-1 "
           >
             Login
           </button>
           <button
             onClick={() => openModal()}
-            className="inline-flex rounded-[8px] flex-col items-center justify-center mr-4 gap-10 p-2 h-50 text-white flex-shrink-0 rounded-10 bg-custom-blue transition-transform transform hover:-translate-y-1"
+            className="inline-flex w-full mt-5 rounded-[8px] flex-col items-center justify-center mr-4 gap-10 text-[22px] p-2 h-50 text-white flex-shrink-0 rounded-10 bg-custom-blue transition-transform transform hover:-translate-y-1"
           >
-            {" "}
-            Sign up
+            Sign up{" "}
           </button>
         </div>
       </div>
