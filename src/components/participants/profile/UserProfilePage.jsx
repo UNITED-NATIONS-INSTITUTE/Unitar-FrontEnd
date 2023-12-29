8a4b30cf447390707539afa80e7ae2a55eec4626
Isaranwa <pennyisaranwa@gmail.com>
2023-12-29 22:55:02 +0300

    Refactor and style the user profile page

--- src/components/participants/profile/UserProfilePage.jsx
+++ src/components/participants/profile/UserProfilePage.jsx
@@ -39,30 +39,45 @@ const UserProfilePage = () => {
             <h1 className="mt-3 mb-2 text-gray-600 font-semibold">
               Profile Details
             </h1>
-            <div className="flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+            <div className="flex flex-col justify-start bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
               <p>
-                <strong className="text-custom-blue px-4">First Name:</strong>{" "}
-                {partProfile.first_name}
+                <strong className="text-custom-blue px-4">Full Name:</strong>{" "}
+                {partProfile.full_name}
               </p>
-              <p className="mt-3">
-                <strong className="text-custom-blue px-4">Last Name:</strong>{" "}
-                {partProfile.last_name}
+              <p className="mt-3 ">
+                <strong className="text-custom-blue px-4 ">Email:</strong>{" "}
+                {partProfile.email}
               </p>
-              <p className="mt-3">
-                <strong className="text-custom-blue px-4">Residence:</strong>{" "}
-                {partProfile.residence}
+              <p className="mt-3 ">
+                <strong className="text-custom-blue px-4">City:</strong>{" "}
+                {partProfile.city}
               </p>
-            </div>
-            <div className="mt-5 flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
-              <p className="mt-3">
-                <strong className="text-custom-blue px-3">Gender:</strong>{" "}
+              <p className="mt-3 ">
+                <strong className="text-custom-blue px-4">Gender:</strong>{" "}
                 {partProfile.gender}
               </p>
-              <p className="mt-3">
-                <strong className="text-custom-blue px-3">
+              <p className="mt-3 ">
+                <strong className="text-custom-blue px-4">
                   Date of Birth:
                 </strong>{" "}
                 {partProfile.date_of_birth}
+              </p>
+            </div>
+            <div className="mt-5 mb-5 flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+              <p className="mt-3">
+                <strong className="text-custom-blue px-3">
+                  Learning Pathway
+                </strong>
+              </p>
+              <p className="px-3 text-sm mt-2"> {partProfile.pathway}</p>
+              <p className="mt-3">
+                <strong className="text-custom-blue px-3">
+                  Hackathon Theme
+                </strong>{" "}
+              </p>
+              <p className="px-3 text-sm mt-2">
+                {" "}
+                {partProfile.hackathon_theme}
               </p>
             </div>
           </div>
