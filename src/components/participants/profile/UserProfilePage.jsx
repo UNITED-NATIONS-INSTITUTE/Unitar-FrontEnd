57dda47599c1fe32471b1bb4e329d54413f5c9e8
rachealmuchiri <rachealloveo6@gmail.com>
2023-12-31 03:06:15 +0300

    made the page responsive

--- src/components/participants/profile/UserProfilePage.jsx
+++ src/components/participants/profile/UserProfilePage.jsx
@@ -9,13 +9,13 @@ const UserProfilePage = () => {
   const partProfile = useSelector(selectCurrentParticipantDetail);
 
   return (
-    <div className="md:right-side min-h-screen bg-pattern">
-      <div className="md:ml-80">
+    <div className="lg:right-side min-h-screen md:bg-pattern">
+      <div className="lg:ml-80">
         <h1 className="text-gray-600 text-[24px] font-bold py-3">
           User Profile
         </h1>
-        <div className="flex flex-row gap-[100px]">
-          <div className="mt-3 flex justify-center bg-[#f0f6ff] w-[400px] h-[350px] rounded-md shadow-lg px-2 py-4">
+        <div className="lg:flex flex-row gap-[100px]">
+          <div className="mt-3 flex justify-center bg-[#f0f6ff] lg:w-[400px] h-[350px] rounded-md shadow-lg px-2 py-4">
             <div>
               <Avatar
                 alt="Profile pic"
@@ -36,46 +36,49 @@ const UserProfilePage = () => {
             </div>
           </div>
           <div className="mt-10">
-            <h1 className="mt-3 mb-2 text-gray-600 font-semibold">
+            <h1 className="mt-3 mb-2 text-gray-600 font-semibold lg:text-sm md:text-[20px] text-[16px]">
               Profile Details
             </h1>
-            <div className="flex flex-col justify-start bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
-              <p>
+            <div className="flex flex-col justify-start bg-[#f0f6ff] lg:w-[400px] rounded-md shadow-lg px-2 py-4">
+              <p className=" lg:text-sm md:text-[20px] text-[16px]">
                 <strong className="text-custom-blue px-4">Full Name:</strong>{" "}
                 {partProfile.full_name}
               </p>
-              <p className="mt-3 ">
+              <p className="mt-3 lg:text-sm md:text-[20px] text-[16px] ">
                 <strong className="text-custom-blue px-4 ">Email:</strong>{" "}
                 {partProfile.email}
               </p>
-              <p className="mt-3 ">
+              <p className="mt-3 lg:text-sm md:text-[20px] text-[16px] ">
                 <strong className="text-custom-blue px-4">City:</strong>{" "}
                 {partProfile.city}
               </p>
-              <p className="mt-3 ">
+              <p className="mt-3 lg:text-sm md:text-[20px] text-[16px]">
                 <strong className="text-custom-blue px-4">Gender:</strong>{" "}
                 {partProfile.gender}
               </p>
-              <p className="mt-3 ">
+              <p className="mt-3 lg:text-sm md:text-[20px] text-[16px] ">
                 <strong className="text-custom-blue px-4">
                   Date of Birth:
                 </strong>{" "}
                 {partProfile.date_of_birth}
               </p>
             </div>
-            <div className="mt-5 mb-5 flex flex-col bg-[#f0f6ff] w-[400px] rounded-md shadow-lg px-2 py-4">
+            <div className="mt-5 mb-5 flex flex-col bg-[#f0f6ff] lg:w-[400px] rounded-md shadow-lg px-2 py-4">
               <p className="mt-3">
-                <strong className="text-custom-blue px-3">
+                <strong className="text-custom-blue px-3 lg:text-sm md:text-[20px] text-[16px]">
                   Learning Pathway
                 </strong>
               </p>
-              <p className="px-3 text-sm mt-2"> {partProfile.pathway}</p>
+              <p className="px-3 lg:text-sm md:text-[20px] text-[16px] mt-2">
+                {" "}
+                {partProfile.pathway}
+              </p>
               <p className="mt-3">
-                <strong className="text-custom-blue px-3">
+                <strong className="text-custom-blue px-3 lg:text-sm md:text-[20px] text-[16px]">
                   Hackathon Theme
                 </strong>{" "}
               </p>
-              <p className="px-3 text-sm mt-2">
+              <p className="px-3 lg:text-sm md:text-[20px] text-[16px]  mt-2">
                 {" "}
                 {partProfile.hackathon_theme}
               </p>
