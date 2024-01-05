8fca75827f76f9ba256b0586514159a3f6daa04f
Isaranwa <pennyisaranwa@gmail.com>
2024-01-05 17:54:31 +0300

    Add the banner video component

--- src/components/common/LandingPage.jsx
+++ src/components/common/LandingPage.jsx
@@ -5,6 +5,7 @@ import ForParticipants from "./ForParticipants";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
 import BasicModal from "./SignUpModal";
+import BannerVideo from "./BannerVideo";
 const LandingPage = () => {
   const [openSignUpModal, setOpenSignUpModal] = useState(false);
   const openModal = () => setOpenSignUpModal(true);
@@ -18,10 +19,10 @@ const LandingPage = () => {
         <div className="w-full h-screen text-center relative ">
           <div className=" mx-auto p-2 flex justify-center">
             <div className="mt-[60px]">
-              <h1 className=" text-[#000] items-center text-center  lg:text-6xl font-extrabold leading-11 tracking-wider  ">
+              <h1 className="animated-text text-[#000] items-center text-center  lg:text-6xl font-extrabold leading-11 tracking-wider  ">
                 Unleashing Innovation, <br /> One Hackathon at a Time!
               </h1>
-              <p className="text-center lg:w-[588px] mt-7 ml-20 font-normal">
+              <p className="text-center lg:w-[588px] mt-7 ml-20 font-normal text-[#4d4d4d]">
                 Join a global community of thinkers, dreamers, and doers.
                 Whether you're here to conquer challenges or host groundbreaking
                 hackathons, this is where innovation takes center stage.
@@ -31,14 +32,14 @@ const LandingPage = () => {
           <div className="flex gap-10 items-center justify-center md:w-full mt-[100px]">
             <button
               onClick={() => navigate("/part-signup")}
-              className="bg-[#089BD9] rounded-[10px]  text-white py-4 px-[20px] lg:w-[239px]  transition-transform transform hover:-translate-y-1"
+              className="bg-[#089BD9] rounded   text-white py-4 px-[20px] lg:w-[239px]  transition-transform transform hover:-translate-y-1"
             >
               For Participants
             </button>
 
             <button
               onClick={() => navigate("/org-signup")}
-              className="py-4 px-[20px]  rounded-[10px] border-2 lg:w-[239px] border-[#089BD9]  transition-transform transform hover:-translate-y-1"
+              className="py-4 px-[20px] rounded  border-2 lg:w-[239px] border-[#089BD9]  transition-transform transform hover:-translate-y-1"
             >
               For Organisers
             </button>
@@ -46,20 +47,17 @@ const LandingPage = () => {
           <div className="absolute hidden lg:block rounded-full  filter bottom-[230px]  right-[180px]  ">
             <img
               src={man2}
-              className="rounded-full hidden lg:block  bg-[#00609E]  border-white border-4 opacity-4 w-[80px] "
+              className="rounded-full hidden lg:block    border-[#b71079] border-2  w-[80px] "
             />
           </div>
-          <div className="absolute hidden lg:block  w-269 h-269 flex-shrink-0  border-white border-4 rounded-full opacity-4 bg-blue-500  filter left-[150px]  bottom-[50px] ">
-            <img src={man1} />
-          </div>
-          <div className="absolute lg:flex  left-[400px] mt-[120px]  hidden  flex-col justify-center items-center gap-4 p-4 w-[163px] transform -rotate-5 border-2 border-solid border-transparent rounded-10  ">
-            <img src={frame} />
-          </div>
-          <div className="absolute  left-[760px] mt-[24px]   w-[400px]  rounded-10  ">
-            <img src={views} />
+          <div className="absolute hidden lg:block    left-[150px]  bottom-[80px] ">
+            <img
+              src={man1}
+              className="rounded-full hidden lg:block    border-[#db3b14] border-2 4 w-[90px] "
+            />
           </div>
         </div>
-        <Banner />
+        <BannerVideo />
         <ForParticipants />
       </div>
     </div>
