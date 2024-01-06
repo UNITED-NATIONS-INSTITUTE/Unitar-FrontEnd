d11509f543a8cd3adab0ffeb314a3b8d16988e87
Isaranwa <pennyisaranwa@gmail.com>
2024-01-06 15:27:37 +0300

    Refactor the styling of the banner content

--- src/components/common/FeatureBanner.jsx
+++ src/components/common/FeatureBanner.jsx
@@ -1,14 +1,17 @@
-// FeatureBanner.jsx
 import React from "react";
 
 const FeatureBanner = ({ icon, heading, text }) => {
   return (
-    <div className="ml-20  ">
-      <img src={icon} alt="Feature Icon" className="w-15 h-10 " />
-      <h4 className="text-white text-lg text-left font-inter font-bold mt-4">
+    <div className="ml-20 banner-container">
+      <div className="bg-[rgb(255,255,255,0.9)] rounded w-[60px] h-[60px] px-2 py-2">
+        {" "}
+        <img src={icon} alt="Feature Icon" className="w-10 h-10 " />
+      </div>
+
+      <h4 className="text-custom-blue text-lg text-left font-inter font-bold mt-4">
         {heading}
       </h4>
-      <p className="text-white w-[314px] text-left mt-2">{text}</p>
+      <p className="text-white w-[308px] text-left mt-2">{text}</p>
     </div>
   );
 };
